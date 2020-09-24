d312c4dba74b44fb2ca9a4a3a344a53a135ae0d9
Philip Sakievich <psakiev@sanida.gov>
2020-09-24 14:23:45 -0600

    Push to gitlab

---
+++ repos/exawind/packages/nalu-wind/package.py
@@ -0,0 +1,26 @@
+from spack import *
+from spack.pkg.builtin.nalu_wind import NaluWind as bNaluWind
+
+class NaluWind(bNaluWind, CudaPackage):
+    depends_on('trilinos~shared+cuda+exodus+tpetra+muelu+belos+ifpack2+amesos2+zoltan+stk+boost~superlu-dist+superlu+hdf5+zlib+pnetcdf+shards~hypre@master,develop', when='+cuda')
+    #depends_on('kokkos-nvcc-wrapper', when='+cuda')
+
+    def setup_environment(self, spack_env, run_env):
+        if '+cuda' in self.spec:
+            spack_env.set('NVCC_WRAPPER_DEFAULT_COMPILER', spack_cxx)
+
+    def cmake_args(self):
+        define = CMakePackage.define
+
+        options = super(NaluWind, self).cmake_args()
+
+        if '+cuda' in self.spec:
+            options.append(
+                    define('ENABLE_CUDA', True),
+            )
+            for opt in options:
+                if "DCMAKE_CXX_COMPILER" in opt:
+                    ind = options.index(opt)
+                    options[ind] = define("CMAKE_CXX_COMPILER", self.spec['trilinos'].prefix + "/bin/nvcc_wrapper")
+
+        return options
---
+++ start.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+########################################################
+# Environment stuff
+########################################################
+export SPACK_ROOT=${SPACK_MANAGER}/spack
+export SPACK_CONFIG=${SPACK_MANAGER}/configs/snl-ews
+source ${SPACK_ROOT}/share/spack/setup-env.sh
+
+########################################################
+# Simple scripts for making it easier to use 
+########################################################
+function cspack()
+{
+  spack -C ${SPACK_CONFIG} "$@"
+}
+function spack-switch-config()
+{
+  export SPACK_CONFIG=$1
+}
