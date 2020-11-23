8e5a1325ddbd409d5a6e4ff489b3c3a806a71462
Philip Sakievich <psakiev@sanida.gov>
2020-11-23 10:52:26 -0700

    Add syntax to make nalu-wind build with cuda

--- repos/exawind/packages/nalu-wind/package.py
+++ repos/exawind/packages/nalu-wind/package.py
@@ -6,21 +6,38 @@ from shutil import copyfile
 class NaluWind(bNaluWind, CudaPackage):
     depends_on('kokkos-nvcc-wrapper', when='+cuda')
     depends_on('boost cxxstd=11')
-    generator = 'Ninja'
-    depends_on('ninja', type='build')
+    #generator = 'Ninja'
+    #depends_on('ninja-fortran', type='build')
+    depends_on('trilinos+cuda', when='+cuda')
+    for val in CudaPackage.cuda_arch_values:
+        arch_string='cuda_arch={arch}'.format(arch=val)
+        depends_on('trilinos+wrapper {arch}'.format(arch=arch_string), when=arch_string)
     variant('wind_utils',default=False,
             description='Build wind-utils')
 
+    def setup_build_environment(self, env):
+        spec = self.spec
+        if '+cuda' in spec:
+            env.set('TMPDIR', '/scratch/psakiev/tmp')
+            if '+mpi' in spec:
+                env.set('OMPI_CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
+            else:
+                env.set('CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
+
     def cmake_args(self):
-        if self.spec.variants['test_tol']:
-            self.run_tests = True
+        spec = self.spec
         define = CMakePackage.define
         options = super(NaluWind, self).cmake_args()
+
         options.append(define('CMAKE_EXPORT_COMPILE_COMMANDS',True))
-        if 'wind_utils' in self.spec.variants:
+
+        if '+wind_utils' in spec:
             options.append(define('ENABLE_WIND_UTILS', True))
         else:
             options.append(define('ENABLE_WIND_UTILS', False))
+
+        if  '+cuda' in spec:
+            options.append(define('ENABLE_CUDA', True))
 
         return options
 
