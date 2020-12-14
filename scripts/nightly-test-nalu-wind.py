e8743db0a9b783d04f56ad8123f2e1dbef714b14
psakiev <psakiev@sandia.gov>
2020-12-14 08:17:58 -0700

    Update environment strategy

--- scripts/nightly-test-nalu-wind.py
+++ scripts/nightly-test-nalu-wind.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env spack-python
 from spack.main import SpackCommand
+import pathlib
 import sys
 import os
 
 install = SpackCommand('install')
 env = SpackCommand('env')
-uninstall = SpackCommand('uninstall')
 
 env('activate', '{envName}'.format(envName=sys.argv[1]))
-uninstall('-y','nalu-wind-nightly', fail_on_error=False)
-install('-v','nalu-wind-nightly')
-
-#view = SpackCommand('view')
-# create view replacing previous nightly
+install('--overwrite','-y','nalu-wind-nightly')
+env('view','regenerate')
