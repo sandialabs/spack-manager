3cf05d0b5ce038b17536a2f170d359c805a0f8d2
Philip Sakievich <psakiev@sanida.gov>
2021-06-06 21:46:21 -0600

    Add exawind package

---
+++ repos/exawind/packages/exawind/package.py
@@ -0,0 +1,20 @@
+from spack import *
+
+
+class Exawind(Package):
+    """Dummy package for the exawind suite"""
+
+    homepage = "https://github.com/Exawind"
+    url      = "exawind"
+
+    version('main')
+
+    maintainers = ['jrood-nrel', 'psakiev']
+
+    depends_on('nalu-wind+hypre+tioga+openfast+wind-utils')
+    depends_on('amr-wind+openfast+hypre')
+    depends_on('py-stk')
+    depends_on('trilinos') # for seacas tools
+
+    def install(self, spec, prefix):
+        pass
