66deb18abbbaf3e858f41bd9a3311b02e88d972d
Jon Rood <jon.rood@nrel.gov>
2021-06-29 13:10:29 -0600

    Changes to test script.

--- scripts/test-scripts/test.sh
+++ scripts/test-scripts/test.sh
@@ -8,16 +8,15 @@ cmd() {
 set -e
 cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh"
-set +e
-cmd "sspack repo add ${SPACK_MANAGER}/repos/exawind"
-set -e
+set +e; cmd "sspack repo add ${SPACK_MANAGER}/repos/exawind"; set -e
 cmd "mkdir -p ${SPACK_MANAGER}/environments/exawind"
 cmd "cd ${SPACK_MANAGER}/environments/exawind"
 cmd "sspack env create -d ."
 cmd "cp ${SPACK_MANAGER}/env-templates/darwin.yaml ${SPACK_MANAGER}/environments/exawind/spack.yaml"
 cmd "sspack env activate --sh ."
+set +e; cmd "sspack uninstall -a -y amr-wind"; set -e
 #cmd "spack mirror add e4s https://cache.e4s.io"
 #cmd "sspack buildcache keys -it"
 cmd "sspack -e . concretize -f"
 cmd "sspack -e . install"
-cmd "sspack env deactivate --sh"
+#cmd "sspack env deactivate --sh"
