9b37c3571434d56c41f749c01e097eec271a4f34
Jon Rood <jon.rood@nrel.gov>
2021-06-29 17:13:50 -0600

    Gold file archiving stuff.

--- repos/exawind/packages/amr-wind-developer/package.py
+++ repos/exawind/packages/amr-wind-developer/package.py
@@ -13,14 +13,19 @@ class AmrWindDeveloper(bAmrWind):
         define = CMakePackage.define
         options = super(AmrWindDeveloper, self).cmake_args()
 
-        options.append(define('AMR_WIND_SAVE_GOLDS', True))
-        options.append(define('AMR_WIND_SAVED_GOLDS_DIRECTORY', os.path.join(os.getenv('SPACK_MANAGER'), 'tmp', 'tmp_golds')))
-
         if spec['mpi'].name == 'openmpi':
             options.append(define('MPIEXEC_PREFLAGS', '--oversubscribe'))
 
         if '+compile_commands' in spec:
             options.append(define('CMAKE_EXPORT_COMPILE_COMMANDS',True))
+
+        saved_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'tmp', 'tmp_golds', 'amr-wind')
+        current_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'golds', 'current', 'amr-wind')
+        linked_golds = os.path.join(self.stage.source_path, "test", "AMR-WindGoldFiles")
+        options.append(define('AMR_WIND_SAVE_GOLDS', True))
+        options.append(define('AMR_WIND_SAVED_GOLDS_DIRECTORY', saved_golds))
+        if not os.path.lexists(linked_golds):
+            os.symlink(current_golds, linked_golds)
 
         return options
 
--- scripts/test-scripts/test.sh
+++ scripts/test-scripts/test.sh
@@ -10,7 +10,7 @@ cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh"
 set +e; cmd "sspack repo add ${SPACK_MANAGER}/repos/exawind"; set -e
 cmd "mkdir -p ${SPACK_MANAGER}/environments/exawind"
-cmd "mkdir -p ${SPACK_MANAGER}/tmp/tmp_golds"
+cmd "mkdir -p ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind"
 cmd "cd ${SPACK_MANAGER}/environments/exawind"
 cmd "sspack env create -d ."
 cmd "cp ${SPACK_MANAGER}/env-templates/darwin.yaml ${SPACK_MANAGER}/environments/exawind/spack.yaml"
@@ -21,5 +21,5 @@ set +e; cmd "sspack uninstall -a -y amr-wind-nightly"; set -e
 cmd "sspack -e . concretize -f"
 cmd "sspack -e . install"
 #cmd "sspack env deactivate --sh"
-(set -x; tar -czf ${SPACK_MANAGER}/golds/archived/amr_wind_golds-$(date +%Y-%m-%d-%H-%M).tar.gz -C ${SPACK_MANAGER}/tmp/tmp_golds .)
-cmd "rm -rf ${SPACK_MANAGER}/tmp/tmp_golds"
+(set -x; tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-$(date +%Y-%m-%d-%H-%M).tar.gz -C ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind .)
+cmd "rm -rf ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind"
