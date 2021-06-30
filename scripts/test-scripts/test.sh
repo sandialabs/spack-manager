2038be1f7540e31120b5c3ab08f80d0bd054b8ed
Jon Rood <jon.rood@nrel.gov>
2021-06-30 10:39:17 -0600

    More progress and not using sspack.

--- scripts/test-scripts/test.sh
+++ scripts/test-scripts/test.sh
@@ -8,18 +8,16 @@ cmd() {
 set -e
 cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh"
-set +e; cmd "sspack repo add ${SPACK_MANAGER}/repos/exawind"; set -e
 cmd "mkdir -p ${SPACK_MANAGER}/environments/exawind"
 cmd "mkdir -p ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind"
 cmd "cd ${SPACK_MANAGER}/environments/exawind"
-cmd "sspack env create -d ."
+cmd "spack env create -d ."
 cmd "cp ${SPACK_MANAGER}/env-templates/darwin.yaml ${SPACK_MANAGER}/environments/exawind/spack.yaml"
-cmd "sspack env activate --sh ."
-set +e; cmd "sspack uninstall -a -y amr-wind-nightly"; set -e
+cmd "spack env activate --sh ."
+set +e; cmd "spack uninstall -a -y amr-wind-nightly"; set -e
 #cmd "spack mirror add e4s https://cache.e4s.io"
-#cmd "sspack buildcache keys -it"
-cmd "sspack -e . concretize -f"
-cmd "sspack -e . install"
-#cmd "sspack env deactivate --sh"
+#cmd "spack buildcache keys -it"
+cmd "spack -e . concretize -f"
+cmd "spack -e . install"
 (set -x; tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-$(date +%Y-%m-%d-%H-%M).tar.gz -C ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind .)
 cmd "rm -rf ${SPACK_MANAGER}/tmp/tmp_golds/amr-wind"
