18e55353f9b3262203d25f5d1c34360e6a91b308
Jon Rood <jon.rood@nrel.gov>
2021-08-25 11:39:56 -0400

    Make install exawind script more general.

---
+++ scripts/install-exawind.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+set -e
+
+MYPWD=${PWD}
+
+if [ "${SPACK_MANAGER_MACHINE}" == 'summit' ]; then
+  cmd "module load python/3.7.7"
+  cmd "module unload xl"
+  cmd "module load gcc/10.2.0"
+  cmd "mpicc --version"
+fi
+
+cmd "export TMPDIR=${MYPWD}/../tmp"
+cmd "export SPACK_MANAGER=${MYPWD}/.."
+cmd "source ${SPACK_MANAGER}/start.sh"
+cmd "mkdir -p ${SPACK_MANAGER}/environments/exawind"
+cmd "cd ${SPACK_MANAGER}/environments/exawind"
+cmd "spack env create -d ."
+cmd "cp ${SPACK_MANAGER}/env-templates/${SPACK_MANAGER_MACHINE}.yaml ${SPACK_MANAGER}/environments/exawind/spack.yaml"
+cmd "spack env activate --sh ."
+cmd "spack -e . concretize -f"
+cmd "spack -e . install --no-cache"
