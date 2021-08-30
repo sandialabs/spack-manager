672b1cd6474239127eee531e8771812240a77199
psakiev <psakiev@sandia.gov>
2021-08-30 16:46:40 -0600

    Make Exawind inherit again

--- repos/exawind/packages/exawind/package.py
+++ repos/exawind/packages/exawind/package.py
@@ -1,40 +1,19 @@
 from spack import *
+from spack.pkg.builtin.exawind import Exawind as bExawind
 import os
 
 
-class Exawind(CMakePackage):
-    """Multi-application driver for Exawind project."""
-
-    homepage = "https://github.com/Exawind/exawind-driver"
-    git = "https://github.com/Exawind/exawind-driver.git"
-
-    maintainers = ['jrood-nrel']
-
-    tags = ['ecp', 'ecp-apps']
-
-    version('master', branch='main')
-
+class Exawind(bExawind):
     variant('asan', default=False,
             description='turn on address sanitizer')
-
-    depends_on('trilinos+stk')
-    depends_on('tioga+shared~nodegid')
-    depends_on('nalu-wind+hypre+openfast+tioga+wind-utils')
-    depends_on('amr-wind+hypre+mpi+netcdf+openfast')
-    depends_on('openfast+cxx+shared@2.6.0')
-    depends_on('yaml-cpp@0.6:')
+    depends_on('ninja', type='build')
+    generator = 'Ninja'
 
     def cmake_args(self):
         spec = self.spec
 
-        args = [
-            self.define('Trilinos_DIR', spec['trilinos'].prefix),
-            self.define('TIOGA_DIR', spec['tioga'].prefix),
-            self.define('Nalu-Wind_DIR', spec['nalu-wind'].prefix),
-            self.define('AMR-Wind_DIR', spec['amr-wind'].prefix),
-            self.define('OpenFAST_DIR', spec['openfast'].prefix),
-            self.define('YAML-CPP_DIR', spec['yaml-cpp'].prefix)
-        ]
+        args = super(Exawind, self).cmake_args()
+
         if 'dev_path' in spec:
             args.append(define('CMAKE_EXPORT_COMPILE_COMMANDS',True))
 
