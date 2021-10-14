05b32878240dc1e46111891d039a5085d7d39dc4
Jon Rood <jon.rood@nrel.gov>
2021-10-14 14:45:25 -0600

    Fix mistake in hypre package.

--- repos/exawind/packages/hypre/package.py
+++ repos/exawind/packages/hypre/package.py
@@ -5,7 +5,7 @@ import os
 class Hypre(bHypre):
     def _configure_args(self):
         spec = self.spec
-        options = super(Hypre, self).configure_args()                        
+        options = super(Hypre, self)._configure_args()                        
                                                                            
         if '+cuda' in self.spec:
             options.append('--enable-cusparse')
