5a3a9109b3b5771a44a0399745b685f6915b482d
psakiev <psakiev@sandia.gov>
2021-10-20 12:39:19 -0600

    Update unit tests

--- tests/test_create_machine_spack_environment.py
+++ tests/test_create_machine_spack_environment.py
@@ -2,6 +2,7 @@ from tempfile import TemporaryDirectory
 import filecmp
 import os
 import create_machine_spack_environment as cmse
+import pytest
 
 
 def test_basicDirectoryProperties():
@@ -12,3 +13,12 @@ def test_basicDirectoryProperties():
         cmse.CreateEnvDir(args)
         assert os.path.isfile(os.path.join(tmpdir, 'machine_config.yaml'))
 
+
+def test_failsWithAnUnregisteredMachine():
+    with TemporaryDirectory as tmpdir:
+        args = cmse.parse(['-d', tmpdir, '-m', 'theGOAT_HPC'])
+
+        with pytest.raises(RuntimeError) as einfo:
+            cmse.CreateEnvDir(args)
+
+        assert 'Host not setup in spack-manager' in str(einfo.value)
