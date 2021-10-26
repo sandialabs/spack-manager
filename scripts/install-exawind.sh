da46ec5d25ed54a425dcbc7487ea31ee226fa943
Jon Rood <jon.rood@nrel.gov>
2021-10-26 15:57:01 -0600

    Add mixed CPU/GPU exawind-driver capability for Eagle and Summit.

--- scripts/install-exawind.sh
+++ scripts/install-exawind.sh
@@ -12,26 +12,34 @@ set -e
 
 MYPWD=${PWD}
 
+cmd "export TMPDIR=${MYPWD}/../tmp"
+cmd "export SPACK_MANAGER=${MYPWD}/.."
+cmd "source ${SPACK_MANAGER}/start.sh"
+
 if [ "${SPACK_MANAGER_MACHINE}" == 'summit' ]; then
   cmd "module unload xl"
   cmd "module load python/3.7.7"
   cmd "module load binutils"
+elif [ "${SPACK_MANAGER_MACHINE}" == 'eagle' ]; then
+  cmd "source /nopt/nrel/ecom/hpacf/env.sh"
+  cmd "module load binutils"
 fi
 
-cmd "export TMPDIR=${MYPWD}/../tmp"
-cmd "export SPACK_MANAGER=${MYPWD}/.."
-cmd "source ${SPACK_MANAGER}/start.sh"
-if [ "${SPACK_MANAGER_MACHINE}" == 'eagle' ]; then
+if [ "${SPACK_MANAGER_MACHINE}" == 'eagle' || "${SPACK_MANAGER_MACHINE}" == 'summit' ]; then
   cmd "create_machine_spack_environment.py -y ${SPACK_MANAGER}/env-templates/${SPACK_MANAGER_MACHINE}.yaml -d ${SPACK_MANAGER}/environments/exawind"
 else
   cmd "create_machine_spack_environment.py -s exawind+hypre -d ${SPACK_MANAGER}/environments/exawind"
 fi
+
 cmd "cd ${SPACK_MANAGER}/environments/exawind"
 cmd "spack env activate -d ."
+
 if [ "${SPACK_MANAGER_MACHINE}" == 'darwin' ]; then
   cmd "spack compiler find"
 fi
+
 cmd "spack concretize -f"
+
 for i in {1..2}; do
-  cmd "spack install --dont-restage --keep-stage" &
+  cmd "spack install" &
 done; wait
