f6dedde884ac14b7494057c618a6989a0cdeaaba
psakievich <psakiev@sandia.gov>
2021-10-27 15:20:45 -0600

    Comment out the gold saving logic in amr-wind

--- repos/exawind/packages/amr-wind/package.py
+++ repos/exawind/packages/amr-wind/package.py
@@ -24,14 +24,14 @@ class AmrWind(bAmrWind):
         if '+mpi' in spec:
             options.append(define('MPI_ROOT', spec['mpi'].prefix))
 
-        saved_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'tmp', 'tmp_golds', 'amr-wind')
-        current_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'golds', 'current', 'amr-wind')
-        linked_golds = os.path.join(self.stage.source_path, "test", "AMR-WindGoldFiles")
+        # saved_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'tmp', 'tmp_golds', 'amr-wind')
+        # current_golds = os.path.join(os.getenv('SPACK_MANAGER'), 'golds', 'current', 'amr-wind')
+        # linked_golds = os.path.join(self.stage.source_path, "test", "AMR-WindGoldFiles")
         # Make this a variant in the future
-        options.append(define('AMR_WIND_SAVE_GOLDS', False))
-        options.append(define('AMR_WIND_SAVED_GOLDS_DIRECTORY', saved_golds))
-        if not os.path.lexists(linked_golds):
-            os.symlink(current_golds, linked_golds)
+        # options.append(define('AMR_WIND_SAVE_GOLDS', False))
+        # options.append(define('AMR_WIND_SAVED_GOLDS_DIRECTORY', saved_golds))
+        # if not os.path.lexists(linked_golds):
+        #     os.symlink(current_golds, linked_golds)
 
         return options
 
