1c5ab387bdacd607a3270333e5fe60e1ae93858d
Jon Rood <jon.rood@nrel.gov>
2021-10-28 14:02:38 -0400

    Fix M4 texinfo dependency for Summit.

--- repos/exawind/packages/m4/package.py
+++ repos/exawind/packages/m4/package.py
@@ -3,5 +3,5 @@ from spack.pkg.builtin.m4 import M4 as bM4
 import os
 
 class M4(bM4):
-    if os.environ['SPACK_MANAGER_MACHINE'] != 'summit':
+    if os.environ['SPACK_MANAGER_MACHINE'] == 'summit':
         depends_on('texinfo', type='build')
