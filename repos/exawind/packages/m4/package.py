c5f11699e8a303b060dc2016cb538fd6afcb2124
Jon Rood <jon.rood@nrel.gov>
2021-10-28 12:46:40 -0400

    Narrow custom m4 texinfo dependency to only Summit.

--- repos/exawind/packages/m4/package.py
+++ repos/exawind/packages/m4/package.py
@@ -3,4 +3,5 @@ from spack.pkg.builtin.m4 import M4 as bM4
 import os
 
 class M4(bM4):
-    depends_on('texinfo', type='build')
+    if os.environ['SPACK_MANAGER_MACHINE'] != 'summit':
+        depends_on('texinfo', type='build')
