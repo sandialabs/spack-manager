b659daf450793dd4c68b6e762d4db7f65faf9f36
psakiev <psakiev@sandia.gov>
2021-10-29 22:44:37 -0600

    Conveert environment creation to new framework

--- spack-scripting/scripting/cmd/manager.py
+++ spack-scripting/scripting/cmd/manager.py
@@ -1,5 +1,6 @@
 import manager_cmds.hello_world
 import manager_cmds.find_machine
+import manager_cmds.create_env
 
 description = "commands that are specific to spack-manager"
 section = "spack-manager"
@@ -12,6 +13,7 @@ def setup_parser(subparser):
     sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='manager_command')
     manager_cmds.hello_world.add_command(sp, _subcommands)
     manager_cmds.find_machine.add_command(sp, _subcommands)
+    manager_cmds.create_env.add_command(sp, _subcommands)
 
 
 def manager(parser, args):
