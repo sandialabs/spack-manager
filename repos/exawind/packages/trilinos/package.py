4baf24e12580e406506673ce47b7119a42c1ee63
Jon Rood <jon.rood@nrel.gov>
2021-11-01 11:01:05 -0600

    Update Spack which has our necessary modifications for trilinos and kokkos-nvcc-wrapper.

--- repos/exawind/packages/trilinos/package.py
+++ repos/exawind/packages/trilinos/package.py
@@ -5,18 +5,3 @@ class Trilinos(bTrilinos):
     if os.environ['SPACK_MANAGER_MACHINE'] != 'summit':
         depends_on('ninja', type='build')
         generator = 'Ninja'
-
-    def setup_build_environment(self, env):
-        spec = self.spec
-        if '+cuda' in spec and '+wrapper' in spec:
-            if '+mpi' in spec:
-                env.set('OMPI_CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-                env.set('MPICH_CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-                env.set('MPICXX_CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-            else:
-                env.set('CXX', spec["kokkos-nvcc-wrapper"].kokkos_cxx)
-        # Workaround for segfaults with IPO
-        if '%intel' in spec and '+stk' in spec:
-            for cc in "CXX C F LD".split():
-                env.append_flags(cc + "FLAGS", '-no-ipo')
-
