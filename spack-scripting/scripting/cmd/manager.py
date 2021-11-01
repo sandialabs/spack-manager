afeb1b99cd0b904b12e63f08ac7daf431caf8b7f
psakiev <psakiev@sandia.gov>
2021-11-01 14:41:08 -0600

    Add manager develop
    Add an option to have spack manager develop
    and do our own cloning that way

--- spack-scripting/scripting/cmd/manager.py
+++ spack-scripting/scripting/cmd/manager.py
@@ -1,4 +1,5 @@
 import manager_cmds.create_env
+import manager_cmds.develop
 import manager_cmds.find_machine
 
 description = "commands that are specific to spack-manager"
@@ -9,9 +10,11 @@ _subcommands = {}
 
 
 def setup_parser(subparser):
-    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='manager_command')
-    manager_cmds.find_machine.add_command(sp, _subcommands)
+    sp = subparser.add_subparsers(metavar='spack-manager commands',
+                                  dest='manager_command')
     manager_cmds.create_env.add_command(sp, _subcommands)
+    manager_cmds.develop.add_command(sp, _subcommands)
+    manager_cmds.find_machine.add_command(sp, _subcommands)
 
 
 def manager(parser, args):
