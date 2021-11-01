b5b6495552a8e6eb17479f610490a5dc4cf26d2c
psakiev <psakiev@sandia.gov>
2021-11-01 14:53:45 -0600

    Remove test file

--- spack-scripting/scripting/cmd/manager_cmds/hello_world.py
+++
@@ -1,7 +0,0 @@
-def hello_world(parser, args):
-    print('Hello World. I am Spack-Manager. Inside manager')
-
-
-def add_command(parser, command_dict):
-    parser.add_parser('hello-world', help='simple call to hello-world')
-    command_dict['hello-world'] = hello_world
