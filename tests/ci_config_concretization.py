1f383729b120558a0e7fbb6a4f9c87a7f249040b
psakiev <psakiev@sandia.gov>
2021-11-01 16:42:18 -0600

    Update CI to use our templates

--- tests/ci_config_concretization.py
+++ tests/ci_config_concretization.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env spack-python
 from tempfile import TemporaryDirectory
 from find_machine import machine_list
-import create_machine_spack_environment as cmse
 import spack.environment as env
+import spack.main
+import argparse
+
+manager = spack.main.SpackCommand('manager')
 
 
-def check_config_concretizations(name):
+def check_config_concretizations(name, ref_yaml):
     print('Concretization test for', name)
     with TemporaryDirectory() as tmpdir:
-        args = cmse.Parse(['-d', tmpdir, '-m', name])
         try:
-            cmse.CreateEnvDir(args)
+            manager('create-env', '-y', ref_yaml, '-d', tmpdir, '-m', name)
             with env.Environment(tmpdir) as this_env:
                 this_env.concretize()
         except Exception as err:
@@ -20,14 +22,29 @@ def check_config_concretizations(name):
         print(name, 'concretized succesfully')
 
 
-if __name__ == '__main__':
+def run_tests(args):
     failure = False
     machine_names = list(machine_list.keys())
+    matrix_test_machines = ['eagle', 'summit']
+
+    if '_matrix.yaml' in args.yaml:
+        machine_names = matrix_test_machines
+    else:
+        machine_names = list(set(machine_names) - set(matrix_test_machines))
+
     for name in machine_names:
         try:
-            check_config_concretizations(name)
+            check_config_concretizations(name, args.yaml)
         except(Exception) as e:
             print(e)
             failure = True
             continue
     exit(failure)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Arguments for testing concretization')
+    parser.add_argument('-y', '--yaml', required=True,
+                        help='reference yaml for the environment')
+    args = parser.parse_args()
+    run_tests(args)
