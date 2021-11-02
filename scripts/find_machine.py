3a99729597872f5a75ae43e1a52286cee6135cc3
psakiev <psakiev@sandia.gov>
2021-11-02 10:59:35 -0600

    Generalize find_machine for SNL HPC's

--- scripts/find_machine.py
+++ scripts/find_machine.py
@@ -1,58 +1,6 @@
 #!/usr/bin/env spack-python
 
-import sys
-import os
-import socket
-
-
-def is_cee(hostname):
-    known_hosts = ('cee', 'ews', 'ecs', 'hpws')
-    for k in known_hosts:
-        if k in hostname:
-            return True
-    return False
-
-
-"""
-setup a dictionary with a key for machine name and checker function
-for value. the checker function should return true for the machine
-match
-"""
-machine_list = {
-    'cee': lambda: is_cee(socket.gethostname()),
-    'eagle': lambda: os.environ['NREL_CLUSTER'] == 'eagle',
-    'rhodes': lambda: os.environ['NREL_CLUSTER'] == 'rhodes',
-    'summit': lambda: os.environ['LMOD_SYSTEM_NAME'] == 'summit',
-    'skybridge': lambda: 'skybridge' in socket.gethostname(),
-    'ascicgpu': lambda: 'ascicgpu' in socket.gethostname(),
-    'darwin': lambda: sys.platform == 'darwin',
-}
-
-
-def find_machine():
-    for machine, i_am_this_machine in machine_list.items():
-        """
-        Since we don't expect uniform environments on all machines
-        we bury our checks in a try/except
-        """
-        try:
-            if i_am_this_machine():
-                return machine
-        except(KeyError):
-            """
-            expect key errors when an environment variable is not defined
-            so these are skipped
-            """
-            pass
-        except:
-            """
-            all other errors will be raised and kill the program
-            we can add more excpetions to the pass list as needed
-            in the future
-            """
-            raise
-    return 'NOT-FOUND'
-
+from manager_cmds.find_machine import find_machine
 
 if __name__ == '__main__':
     print(find_machine())
--- tests/ci_config_concretization.py
+++ tests/ci_config_concretization.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env spack-python
 from tempfile import TemporaryDirectory
-from find_machine import machine_list
+from manager_cmds.find_machine import machine_list
 import spack.environment as env
 import spack.main
 import argparse
