4fecf7012f69294cff75bc03f2a4df784e895a25
Philip Sakievich <psakiev@sandia.gov>
2021-11-03 08:00:11 -0600

    flake

--- scripts/create-nightly-test-env.py
+++ scripts/create-nightly-test-env.py
@@ -11,9 +11,14 @@ add_spec = spack.main.SpackCommand('add')
 
 def parse(args):
     parser = argparse.ArgumentParser('Create nightly test environments')
-    parser.add_argument('-d', '--directory', required=True, help='directory to create file')
-    parser.add_argument('-test-nalu', dest='test_nalu', action='store_true', help='test nalu')
-    parser.add_argument('-nalu-wind-vars', help='string of spack variants for nalu-wind')
+    parser.add_argument(
+                       '-d', '--directory',
+                        required=True, help='directory to create file')
+    parser.add_argument(
+                       '-test-nalu',
+                       dest='test_nalu', action='store_true', help='test nalu')
+    parser.add_argument('-nalu-wind-vars',
+                        help='string of spack variants for nalu-wind')
     parser.set_defaults(test_nalu=False)
     return parser.parse_args(args)
 
@@ -23,7 +28,9 @@ def variant_factory(spec_string):
         add_spec('trilinos')
         branch = 'master' if 'triilinos@master' in spec_string else 'develop'
         print('cloning trilinos {b}'.format(b=branch))
-        manager('develop', '-rb', 'https://github.com/trilinos/trilinos', branch, 'trilinos@{b}'.format(b=branch))
+        manager('develop', '-rb',
+                'https://github.com/trilinos/trilinos',
+                branch, 'trilinos@{b}'.format(b=branch))
 
     if '+openfast' in spec_string:
         add_spec('openfast')
@@ -41,16 +48,17 @@ def variant_factory(spec_string):
         manager('develop', 'tioga@develop')
 
 
-
 def create_test_env(args):
     if args.test_nalu:
-        manager('create-env', '-d', args.directory, '-s', 'nalu-wind-nightly'+args.nalu_wind_vars)
+        manager('create-env', '-d', args.directory,
+                '-s', 'nalu-wind-nightly' + args.nalu_wind_vars)
         with env.Environment(args.directory):
             test_package = 'nalu-wind-nightly@master'
-            spec = test_package+args.nalu_wind_vars
+            spec = test_package + args.nalu_wind_vars
             variant_factory(spec)
             print('cloning nalu-wind')
-            manager('develop', '-rb', 'https://github.com/Exawind/nalu-wind', 'master', test_package)
+            manager('develop', '-rb',
+                    'https://github.com/Exawind/nalu-wind', 'master', test_package)
 
 
 if __name__ == '__main__':
@@ -59,4 +67,3 @@ if __name__ == '__main__':
     # we could potentially just run the enviornment install after this
     # the catch is our test environments would capture changes in spack-manager
     # this could be good or bad depending on how you look at testing
-
