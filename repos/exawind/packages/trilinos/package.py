4af6b109a6945d3f1e094aecad629252310792fa
psakievich <psakiev@sandia.gov>
2021-11-22 05:53:10 -0700

    Add functionality for cpp17 builds (#56)

--- repos/exawind/packages/trilinos/package.py
+++ repos/exawind/packages/trilinos/package.py
@@ -2,6 +2,7 @@ from spack.pkg.builtin.trilinos import Trilinos as bTrilinos
 import os
 
 class Trilinos(bTrilinos):
+    patch('c++17.patch', when='cxxstd=17')
     if os.environ['SPACK_MANAGER_MACHINE'] != 'summit':
         depends_on('ninja', type='build')
         generator = 'Ninja'
