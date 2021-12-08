5fd579bd2b3f96dd1c99b92f5f193ca0b86e1f5b
Jon Rood <jon.rood@nrel.gov>
2021-12-08 15:22:40 -0700

    Fix testing script.

--- scripts/nrel-nightly-testing.sh
+++ scripts/nrel-nightly-testing.sh
@@ -14,7 +14,9 @@ cmd "spack uninstall -a -y amr-wind-nightly || true"
 cmd "export EXAWIND_TEST_DIR=${SPACK_MANAGER}/environments/exawind"
 cmd "spack manager create-env -d ${EXAWIND_TEST_DIR} -s amr-wind-nightly"
 cmd "spack env activate ${EXAWIND_TEST_DIR}"
-(set -x; tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-$(date +%Y-%m-%d-%H-%M).tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind .)
-cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
 cmd "nice spack concretize -f"
 cmd "nice -n19 ionice -c3 spack install"
+DATE=$(date +%Y-%m-%d-%H-%M)
+cmd "mkdir -p ${SPACK_MANAGER}/golds/archived/amr-wind"
+cmd "tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
+cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
