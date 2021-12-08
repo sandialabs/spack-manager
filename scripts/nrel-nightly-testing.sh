1f8dff5a078cc3741dda917cb54d760e02f323f0
Jon Rood <jon.rood@nrel.gov>
2021-12-08 14:57:26 -0700

    Add saving of amr-wind golds to testing script.

--- scripts/nrel-nightly-testing.sh
+++ scripts/nrel-nightly-testing.sh
@@ -9,9 +9,12 @@ set -e
 
 cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh"
+cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/amr-wind"
 cmd "spack uninstall -a -y amr-wind-nightly || true"
 cmd "export EXAWIND_TEST_DIR=${SPACK_MANAGER}/environments/exawind"
 cmd "spack manager create-env -d ${EXAWIND_TEST_DIR} -s amr-wind-nightly"
 cmd "spack env activate ${EXAWIND_TEST_DIR}"
+(set -x; tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-$(date +%Y-%m-%d-%H-%M).tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind .)
+cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
 cmd "nice spack concretize -f"
 cmd "nice -n19 ionice -c3 spack install"
