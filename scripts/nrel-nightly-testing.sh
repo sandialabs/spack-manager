f470f0554c32c8170ab847f3637ba1d6b7df3d6b
Jon Rood <jon.rood@nrel.gov>
2021-12-08 15:51:59 -0700

    Add templates and update script for full testing.

--- scripts/nrel-nightly-testing.sh
+++ scripts/nrel-nightly-testing.sh
@@ -7,16 +7,30 @@ cmd() {
 
 set -e
 
+# Activate spack-manager
 cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh"
-cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/amr-wind"
-cmd "spack uninstall -a -y amr-wind-nightly || true"
-cmd "export EXAWIND_TEST_DIR=${SPACK_MANAGER}/environments/exawind"
-cmd "spack manager create-env -d ${EXAWIND_TEST_DIR} -s amr-wind-nightly"
-cmd "spack env activate ${EXAWIND_TEST_DIR}"
-cmd "nice spack concretize -f"
-cmd "nice -n19 ionice -c3 spack install"
-DATE=$(date +%Y-%m-%d-%H-%M)
-cmd "mkdir -p ${SPACK_MANAGER}/golds/archived/amr-wind"
-cmd "tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
+
+# Setup gold file directories
 cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
+cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/amr-wind"
+cmd "mkdir -p ${SPACK_MANAGER}/golds/archived/amr-wind"
+
+# Setup and activate Spack environment
+cmd "export EXAWIND_ENV_DIR=${SPACK_MANAGER}/environments/exawind"
+YAML_FILE="${SPACK_MANAGER}/env-templates/exawind_${SPACK_MANAGER_MACHINE}_testing.yaml"
+cmd "spack manager create-env -y ${YAML_FILE} -d ${EXAWIND_ENV_DIR}"
+cmd "spack env activate ${EXAWIND_ENV_DIR}"
+
+# Uninstall packages to test
+cmd "nice -n19 ionice -c3 spack uninstall -a -y exawind-nightly || true"
+cmd "nice -n19 ionice -c3 spack uninstall -a -y naly-wind-nightly || true"
+cmd "nice -n19 ionice -c3 spack uninstall -a -y amr-wind-nightly || true"
+
+# Concretize environment and run tests
+cmd "nice -n19 ionice -c3 spack concretize -f"
+cmd "nice -n19 ionice -c3 spack install"
+
+# Save gold files
+DATE=$(date +%Y-%m-%d-%H-%M)
+cmd "nice -n19 ionice -c3 tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr_wind_golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
