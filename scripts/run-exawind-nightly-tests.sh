9f5ac6cc292d0236c594d4e5d426b47cd34538e3
Jon Rood <jon.rood@nrel.gov>
2021-12-13 21:17:44 -0700

    Add scripts for cron jobs for nightly tests (#68)

--- scripts/run-exawind-nightly-tests.sh
+++ scripts/run-exawind-nightly-tests.sh
@@ -1,5 +1,37 @@
 #!/bin/bash -l
 
+#Scheduled script that invokes the nightly Exawind tests
+
+#Example of cron schedule entries:
+#Exawind update spack-manager
+#0 0 * * * /bin/bash -c "export SPACK_MANAGER=/projects/ecp/exawind/exawind-testing/spack-manager && cd \${SPACK_MANAGER} && \${SPACK_MANAGER}/scripts/update-spack-manager-repo.sh > \${SPACK_MANAGER}/logs/last-spack-manager-repo-update.txt 2>&1"
+#Exawind tests
+#10 0 * * * /bin/bash -c "export SPACK_MANAGER=/projects/ecp/exawind/exawind-testing/spack-manager && cd \${SPACK_MANAGER} && \${SPACK_MANAGER}/spack-manager/scripts/run-exawind-nightly-tests.sh > \${SPACK_MANAGER}/logs/last-exawind-test-script-invocation.txt 2>&1"
+
+cmd() {
+  echo "+ $@"
+  eval "$@"
+}
+
+set -e
+
+if [[ -z ${SPACK_MANAGER} ]]; then
+    echo "SPACK_MANAGER not set so setting it to ${PWD}/spack-manager."
+    cmd "export SPACK_MANAGER=${PWD}"
+else
+    echo "SPACK_MANAGER set to ${SPACK_MANAGER}"
+fi
+
+# Activate spack-manager
+cmd "source ${SPACK_MANAGER}/start.sh"
+
+EXAWIND_TEST_SCRIPT=${SPACK_MANAGER}/scripts/exawind-tests-script.sh
+LOG_DIR=${SPACK_MANAGER}/logs
+cd ${LOG_DIR}
+
+cat > ${EXAWIND_TEST_SCRIPT} << '_EOF'
+#!/bin/bash -l
+
 # Trap and kill background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
@@ -10,10 +42,6 @@ cmd() {
 
 set -e
 
-# Activate spack-manager
-cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
-cmd "source ${SPACK_MANAGER}/start.sh"
-
 # Setup gold file directories
 cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
 cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/amr-wind"
@@ -23,9 +51,9 @@ cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/nalu-wind"
 cmd "mkdir -p ${SPACK_MANAGER}/golds/archived/nalu-wind"
 
 # Uninstall packages to test
-cmd "nice -n19 ionice -c3 spack uninstall -a -y exawind-nightly || true"
-cmd "nice -n19 ionice -c3 spack uninstall -a -y nalu-wind-nightly || true"
-cmd "nice -n19 ionice -c3 spack uninstall -a -y amr-wind-nightly || true"
+cmd "spack uninstall -a -y exawind-nightly || true"
+cmd "spack uninstall -a -y nalu-wind-nightly || true"
+cmd "spack uninstall -a -y amr-wind-nightly || true"
 
 # Setup and activate Spack environment
 cmd "export EXAWIND_ENV_DIR=${SPACK_MANAGER}/environments/exawind"
@@ -35,13 +63,22 @@ cmd "spack manager create-env -y ${YAML_FILE} -d ${EXAWIND_ENV_DIR}"
 cmd "spack env activate ${EXAWIND_ENV_DIR}"
 
 # Concretize environment and run tests
-cmd "nice -n19 ionice -c3 spack concretize -f"
+cmd "spack concretize -f"
 # Parallelize Spack install DAG
 for i in {1..2}; do
-  cmd "nice -n19 ionice -c3 spack install" &
+  cmd "spack install" &
 done; wait
 
 # Save gold files
 DATE=$(date +%Y-%m-%d-%H-%M)
-cmd "nice -n19 ionice -c3 tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
-cmd "nice -n19 ionice -c3 tar -czf ${SPACK_MANAGER}/golds/archived/nalu-wind/nalu-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/nalu-wind ."
+cmd "tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
+cmd "tar -czf ${SPACK_MANAGER}/golds/archived/nalu-wind/nalu-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/nalu-wind ."
+_EOF
+
+if [ "${SPACK_MANAGER_MACHINE}" == 'eagle' ]; then
+  cd ${LOG_DIR} && sbatch -J test-exawind -N 1 -t 4:00:00 -A hfm -p short -o "%x.o%j" --gres=gpu:2 ${EXAWIND_TEST_SCRIPT}
+elif [ "${SPACK_MANAGER_MACHINE}" == 'rhodes' ]; then
+  cd ${LOG_DIR} && nice -n19 ionice -c3 ${EXAWIND_TEST_SCRIPT} &> "test-exawind-$(date +%Y-%m-%d).log"
+elif [ "${SPACK_MANAGER_MACHINE}" == 'darwin' ]; then
+  cd ${LOG_DIR} && nice -n20 ${EXAWIND_TEST_SCRIPT} &> "test-exawind-$(date +%Y-%m-%d).log"
+fi
