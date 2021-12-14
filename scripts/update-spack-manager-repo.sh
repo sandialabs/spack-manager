0a8ba801d53dd91e822c0188a74ad71ad5037650
Jon Rood <jon.rood@nrel.gov>
2021-12-14 08:49:33 -0700

    Fix cron example and update spack-manager repo script. (#69)

--- scripts/update-spack-manager-repo.sh
+++ scripts/update-spack-manager-repo.sh
@@ -2,7 +2,7 @@
 
 # Script for updating the spack-manager repo before it's used in running the tests.
 
-printf "$(date)\n"
-printf "Job is running on ${HOSTNAME}\n"
-printf "\n\nUpdating build-test repo...\n\n"
-(set -x; pwd && git fetch --all && git reset --hard origin/main && git clean -df && git status -uno) 2>&1
+printf "\n$(date)\n"
+printf "\nJob is running on ${HOSTNAME}\n"
+printf "\nUpdating spack-manager repo...\n"
+(set -x; pwd && git fetch --all && git reset --hard origin/main && git clean -df && git submodule update && git status -uno) 2>&1
