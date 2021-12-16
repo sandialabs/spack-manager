bdd51bb2b3d87ff84015ea19cfea0546b2d63ddb
Jon Rood <jon.rood@nrel.gov>
2021-12-16 11:58:54 -0700

    Optimize cloning and updating of trilinos in testing (#75)

--- scripts/run-exawind-nightly-tests.sh
+++ scripts/run-exawind-nightly-tests.sh
@@ -30,7 +30,7 @@ cat > ${EXAWIND_TEST_SCRIPT} << '_EOF'
 #!/bin/bash -l
 
 # Trap and kill background processes
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+#trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 cmd() {
   echo "+ $@"
@@ -41,6 +41,12 @@ set -e
 
 printf "Starting at $(date)\n"
 
+printf "\nSetting up recommended environment for machine...\n"
+ENV_SCRIPT=${SPACK_MANAGER}/configs/${SPACK_MANAGER_MACHINE}/env.sh
+if [[ -f "${ENV_SCRIPT}" ]]; then
+  cmd "source ${ENV_SCRIPT}"
+fi
+
 printf "\nSetting up gold files directories...\n"
 cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/amr-wind"
 cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/amr-wind"
@@ -49,17 +55,28 @@ cmd "rm -rf ${SPACK_MANAGER}/golds/tmp/nalu-wind"
 cmd "mkdir -p ${SPACK_MANAGER}/golds/tmp/nalu-wind"
 cmd "mkdir -p ${SPACK_MANAGER}/golds/archived/nalu-wind"
 
+printf "\nSetting up Spack environoment...\n"
+cmd "export EXAWIND_ENV_DIR=${SPACK_MANAGER}/environments/exawind"
+YAML_FILE="${SPACK_MANAGER}/env-templates/exawind_${SPACK_MANAGER_MACHINE}_tests.yaml"
+cmd "spack manager create-env -y ${YAML_FILE} -d ${EXAWIND_ENV_DIR}"
+
+printf "\nUpdating git repos in selected stage directories...\n"
+cmd "spack env activate -d ${EXAWIND_ENV_DIR}"
+cmd "${SPACK_MANAGER}/scripts/stage-updater.py -e ${SPACK_MANAGER}/environments/exawind"
+cmd "spack env deactivate"
+
 printf "\nUninstall nightly test packages...\n"
 cmd "spack uninstall -a -y exawind-nightly || true"
 cmd "spack uninstall -a -y nalu-wind-nightly || true"
 cmd "spack uninstall -a -y amr-wind-nightly || true"
+cmd "spack uninstall -a -y trilinos || true"
+cmd "spack uninstall -a -y hypre || true"
 
-printf "\nSetting up and activating Spack environoment...\n"
-cmd "export EXAWIND_ENV_DIR=${SPACK_MANAGER}/environments/exawind"
-YAML_FILE="${SPACK_MANAGER}/env-templates/exawind_${SPACK_MANAGER_MACHINE}_tests.yaml"
-cmd "rm -f ${EXAWIND_ENV_DIR}/spack.yaml"
-cmd "spack manager create-env -y ${YAML_FILE} -d ${EXAWIND_ENV_DIR}"
-cmd "spack env activate ${EXAWIND_ENV_DIR}"
+printf "\nConcretizing environment...\n"
+cmd "spack env activate -d ${EXAWIND_ENV_DIR}"
+cmd "spack concretize -f"
+
+# Develop spec stuff that isn't working as desired
 #DEVELOP_SPEC_DIR=${SPACK_MANAGER}/stage/develop-specs/amr-wind-nightly
 #if [[ -d ${DEVELOP_SPEC_DIR} ]]; then
 #  cmd "spack develop -p ${DEVELOP_SPEC_DIR} --no-clone amr-wind-nightly@main"
@@ -72,14 +89,11 @@ cmd "spack env activate ${EXAWIND_ENV_DIR}"
 #else
 #  cmd "spack develop -p ${DEVELOP_SPEC_DIR} --clone hypre@develop"
 #fi
-#cmd "${SPACK_MANAGER}/scripts/spec_updater.py -e ${SPACK_MANAGER}/environments/exawind"
-cmd "spack concretize -f"
 
-# Parallelize Spack install DAG
-printf "\nTests started at: $(date)\n"
-printf "\nspack install\n"
-time (for i in {1..2}; do spack install & done; wait)
-#time spack install
+printf "\nTests started at: $(date)\n\n"
+#printf "spack install --dont-restage --keep-stage\n"
+#time (for i in {1..4}; do spack install --dont-restage --keep-stage & done; wait)
+cmd "time spack install --dont-restage --keep-stage"
 printf "\nTests ended at: $(date)\n"
 
 printf "\nSaving gold files...\n"
@@ -87,7 +101,19 @@ DATE=$(date +%Y-%m-%d-%H-%M)
 cmd "tar -czf ${SPACK_MANAGER}/golds/archived/amr-wind/amr-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/amr-wind ."
 cmd "tar -czf ${SPACK_MANAGER}/golds/archived/nalu-wind/nalu-wind-golds-${DATE}.tar.gz -C ${SPACK_MANAGER}/golds/tmp/nalu-wind ."
 
-printf "\nDone at $(date)"
+STAGE_DIR=$(spack location -S)
+if [ ! -z "${STAGE_DIR}" ]; then
+  #Haven't been able to find another robust way to rm with exclude
+  printf "\nRemoving all unused staged directories...\n"
+  cmd "cd ${STAGE_DIR} && rm -rf resource* spack-stage-b* spack-stage-c* spack-stage-d* spack-stage-f* spack-stage-g* spack-stage-h* spack-stage-i* spack-stage-j* spack-stage-k* spack-stage-l* spack-stage-m* spack-stage-nc* spack-stage-net* spack-stage-o* spack-stage-p* spack-stage-q* spack-stage-r* spack-stage-s* spack-stage-tar* spack-stage-ti* spack-stage-u* spack-stage-v* spack-stage-w* spack-stage-x* spack-stage-y* spack-stage-z*"
+  #Would like something like this
+  #find ${STAGE_DIR}/ -maxdepth 0 -type d -not -name "spack-stage-trilinos*" -exec rm -r {} \;
+fi
+
+printf "\nDeactivating environment...\n"
+cmd "spack env deactivate"
+
+printf "\nDone at $(date)\n"
 _EOF
 
 cmd "chmod u+x ${EXAWIND_TEST_SCRIPT}"
@@ -103,4 +129,4 @@ elif [ "${SPACK_MANAGER_MACHINE}" == 'darwin' ]; then
   (set -x; cd ${LOG_DIR} && nice -n20 ${EXAWIND_TEST_SCRIPT} &> test-exawind-${DATE}.log)
 fi
 
-printf "\nDone at $(date)"
+printf "\nDone at $(date)\n"
---
+++ scripts/stage-updater.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env spack-python
+"""
+This script will cycle over all packages installed
+in an environment and update git source code
+repos for packages with specific names.
+"""
+import spack.environment as ev
+from spack.environment import installed_specs
+import spack.util.executable
+import os
+import sys
+
+git = spack.util.executable.which('git')
+
+
+def GetValidEnvironment(env):
+    try:
+        return ev.read(env)
+    except:
+        try:
+            ev.Environment(env)
+        except:
+            raise ev.SpackEnvironmentErrror(
+                '%s is not a valid environment' % env)
+        finally:
+            return ev.Environment(env)
+    return None
+
+
+def UpdateDevelopmentSpecs(e):
+    package_names = ['trilinos']
+    env = GetValidEnvironment(e)
+    with env:
+        for spec in installed_specs():
+            if spec.package.name in package_names:
+                path = spec.package.stage.source_path
+                version = str(spec.package.version)
+                origin = 'origin/' + version
+                print('Resetting git repo to %s in stage: %s'
+                      % (origin, path), flush=True)
+                os.chdir(path)
+                git('fetch', '--all')
+                git('reset', '--hard', origin)
+                git('clean', '-df')
+                git('submodule', 'update')
+                git('status', '-uno')
+
+
+def Parse(args):
+    parser = argparse.ArgumentParser(
+        description='Cycle spec stages and update them')
+    parser.add_argument(
+        '-e', '--environment', required=False,
+        help='Single environment with spec stages to update')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    args = Parse(sys.argv[1:])
+
+    if args.environment is not None:
+        env = args.environment
+        UpdateDevelopmentSpecs(env)
