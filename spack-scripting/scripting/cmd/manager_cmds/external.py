87b42a5068dc2e46d3b1dcc9a06aeb215b524de7
psakiev <psakiev@sandia.gov>
2021-12-20 10:58:25 -0700

    WIP

--- spack-scripting/scripting/cmd/manager_cmds/external.py
+++ spack-scripting/scripting/cmd/manager_cmds/external.py
@@ -1,3 +1,5 @@
+import os
+
 import spack.cmd
 import spack.cmd.common.arguments as arguments
 
@@ -5,6 +7,9 @@ import spack.cmd.common.arguments as arguments
 def external(parser, args):
     env = spack.cmd.require_active_env(cmd_name='external')
     # check that directory of view exists
+    fpath = os.path.join(args.path, 'external.yaml')
+    if os.path.isfile(fpath):
+        pass
     # search for externals.yaml in the directory
     # copy the file and overwrite any that may exist (or merge?)
     # search for `external.yaml` in the spack:includes and add if it is missing
@@ -15,6 +20,5 @@ def add_command(parser, command_dict):
     external_prsr = parser.add_parser('external', help='tools for configuring precompiled'
                                   ' binaries', conflict_handler='resolve')
     external_prsr.add_argument('-d', '--dependencies', required=False, help='set all the dependencies of this package as externals')
-    arguments.add_common_arguments(external_prsr, ['spec'])
     external_prsr.add_argument('path', help='The location of the external install directory')
     command_dict['external'] = external
