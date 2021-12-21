7f448d477597a25d1316b6b83e75cb37a54fe56c
Jon Rood <jon.rood@nrel.gov>
2021-12-21 14:35:30 -0700

    ASAN suppressions should be set in build environment. Also adding suppressions for Nalu-Wind.

--- repos/exawind/packages/amr-wind/package.py
+++ repos/exawind/packages/amr-wind/package.py
@@ -15,12 +15,9 @@ class AmrWind(bAmrWind, ROCmPackage):
     def setup_build_environment(self, env):
         if '+asan' in self.spec:
             env.append_flags('CXXFLAGS', '-fsanitize=address', '-fno-omit-frame-pointer')
+            env.set("LSAN_OPTIONS", "suppressions={0}".format(join_path(self.package_dir, 'sup.asan')))
         if '%intel' in self.spec:
             env.append_flags('CXXFLAGS', '-no-ipo')
-
-    def setup_run_environment(self, env):
-        if '+asan' in self.spec:
-            env.set("LSAN_OPTIONS", "suppressions={0}".format(join_path(self.package_dir, 'sup.asan')))
 
     def cmake_args(self):
         spec = self.spec
