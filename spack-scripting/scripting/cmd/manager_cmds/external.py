2535ebc2ea572cb3d4b30285c0936276deb3119b
psakiev <psakiev@sandia.gov>
2021-12-20 23:26:55 -0700

    Basic functionality added

--- spack-scripting/scripting/cmd/manager_cmds/external.py
+++ spack-scripting/scripting/cmd/manager_cmds/external.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import llnl.util.tty as tty
 
@@ -7,28 +8,54 @@ from manager_cmds.includes_creator import IncludesCreator
 import spack.cmd
 import spack.cmd.common.arguments as arguments
 import spack.environment as ev
+from spack.environment import config_dict
+
+
+def include_exists(env, name):
+    includes = config_dict(env.yaml).get('include', [])
+    for entry in includes:
+        if entry == name:
+            return True
+    return False
+
+
+def add_include_entry(env, inc, prepend=True):
+    includes = config_dict(env.yaml).get('include', [])
+    if prepend:
+        includes.insert(0, inc)
+    else:
+        includes.append(inc)
+    env.write()
 
 
 def external(parser, args):
     env = ev.active_environment()
     if not env:
-        tty.die('spack manager external requires and active environment')
+        tty.die('spack manager external requires an active environment')
     # check that directory of ext view exists
     fpath = os.path.join(args.path, 'external.yaml')
     if os.path.isfile(fpath):
-        pass
+        # copy the file and overwrite any that may exist (or merge?)
+        if args.name:
+            ext_name = args.name
+        else:
+            ext_name = 'external.yaml'
+
+        if not include_exists(env, ext_name):
+            add_include_entry(env, ext_name)
+
+        # overwrite file for now
+        shutil.copy2(fpath, ext_name)
     else:
         raise tty.die('No external.yaml file has been written'
                       ' to the specified directory')
-    # search for externals.yaml in the directory
-    # copy the file and overwrite any that may exist (or merge?)
-    # search for `external.yaml` in the spack:includes and add if it is missing
-
 
 
 def add_command(parser, command_dict):
     external_prsr = parser.add_parser('external', help='tools for configuring precompiled'
                                   ' binaries', conflict_handler='resolve')
     external_prsr.add_argument('-d', '--dependencies', required=False, help='set all the dependencies of this package as externals')
+    external_prsr.add_argument('-n', '--name', required=False, help='name the new include file for the externals with this name')
+    # todo mutualy exclusive group for blacklist an whitelists
     external_prsr.add_argument('path', help='The location of the external install directory')
     command_dict['external'] = external
