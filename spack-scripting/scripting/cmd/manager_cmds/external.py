7ba36650135a80b50ec8490b4544193a13737b17
psakiev <psakiev@sandia.gov>
2021-12-23 00:35:05 -0700

    Switch to use environments with views and make 1 step process

--- spack-scripting/scripting/cmd/manager_cmds/external.py
+++ spack-scripting/scripting/cmd/manager_cmds/external.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 import llnl.util.tty as tty
 
@@ -30,76 +29,114 @@ def add_include_entry(env, inc, prepend=True):
     env.write()
 
 
+def write_spec(view, spec):
+    template = """  {name}:
+    externals:
+    - spec: {short_spec}
+      prefix: {prefix}
+    buildable: false\n"""
+
+    return template.format(
+        name=spec.name,
+        short_spec=spec.format(
+            '{name}{@version}{%compiler}{variants}{arch=architecture}'),
+        prefix=view.get_projection_for_spec(spec))
+
+
+def create_external_yaml_from_env(path, black_list, white_list, view_key):
+    env = ev.Environment(path)
+
+    # TODO add some error checking for if no views are present
+    view = env.views[view_key]
+
+    specs = env._get_environment_specs()
+    roots = env.roots()
+    data = "packages:\n"
+
+    for s in view.specs_for_view(specs, roots):
+        if black_list:
+            if s.name in black_list:
+                continue
+            else:
+                data += write_spec(view, s)
+        elif white_list:
+            if s.name in white_list:
+                data += write_spec(view, s)
+        else:
+            data += write_spec(view, s)
+    return syaml.load_config(data)
+
+
 def external(parser, args):
-    if args.blacklist or args.whitelist:
-        tty.die('Blake and white lists have not been implemented yet')
     env = ev.active_environment()
     if not env:
         tty.die('spack manager external requires an active environment')
     # check that directory of ext view exists
-    view_path = os.path.join(args.path, 'external.yaml')
-    if os.path.isfile(view_path):
-        # copy the file and overwrite any that may exist (or merge?)
-        if args.name:
-            inc_name = args.name
-        else:
-            inc_name = 'external.yaml'
+    if not ev.is_env_dir(args.path):
+        tty.die('path must point to a spack environment')
 
-        inc_name_abs = os.path.abspath(os.path.join(env.path, inc_name))
-        view_path_abs = os.path.abspath(view_path)
+    # copy the file and overwrite any that may exist (or merge?)
+    if args.name:
+        inc_name = args.name
+    else:
+        inc_name = 'externals.yaml'
 
-        if include_entry_exists(env, inc_name):
-            assert os.path.isfile(inc_name_abs)
-            assert os.path.isfile(view_path_abs)
+    inc_name_abs = os.path.abspath(os.path.join(env.path, inc_name))
+    src = create_external_yaml_from_env(
+        args.path, args.blacklist, args.whitelist, args.view)
 
+    if include_entry_exists(env, inc_name):
+        if args.merge:
             # merge the existing includes with the new one
             # giving precedent to the new data coming in
-            src = spack.config.read_config_file(
-                view_path_abs, spack.config.section_schemas['packages'])
             dest = spack.config.read_config_file(
                 inc_name_abs, spack.config.section_schemas['packages'])
             combined = spack.config.merge_yaml(src, dest)
-
-            with open(inc_name_abs, 'w') as fout:
-                syaml.dump_config(combined, stream=fout,
-                                  default_flow_style=False)
+            final = combined
         else:
-            add_include_entry(env, inc_name)
-            shutil.copy2(view_path_abs, inc_name_abs)
-
+            final = src
     else:
-        raise tty.die('No external.yaml file has been written'
-                      ' to the specified directory')
+        add_include_entry(env, inc_name)
+        final = src
+
+    with open(inc_name_abs, 'w') as fout:
+        syaml.dump_config(final, stream=fout,
+                          default_flow_style=False)
 
 
 def add_command(parser, command_dict):
-    external_prsr = parser.add_parser('external',
-                                      help='tools for configuring precompiled'
-                                      ' binaries', conflict_handler='resolve')
+    ext = parser.add_parser('external',
+                            help='tools for configuring precompiled'
+                            ' binaries', conflict_handler='resolve')
 
-    external_prsr.add_argument('-d',
-                               '--dependencies', required=False,
-                               help='set all the dependencies of this package '
-                               'as externals')
-    external_prsr.add_argument('-n',
-                               '--name', required=False,
-                               help='name the new include file for the '
-                               'externals with this name')
+    ext.add_argument('-d',
+                     '--dependencies', required=False,
+                     help='set all the dependencies of this package '
+                     'as externals')
+    ext.add_argument('-n',
+                     '--name', required=False,
+                     help='name the new include file for the '
+                     'externals with this name')
+    ext.add_argument('-v', '--view', required=False, default='default',
+                     help='name of view to use in the environment')
+    ext.add_argument('--merge', required=False, dest='merge',
+                     action='store_true', help='merge existing yaml files together')
 
-    select_group = external_prsr.add_mutually_exclusive_group()
+    select = ext.add_mutually_exclusive_group()
 
-    select_group.add_argument('-w',
-                              '--whitelist', nargs='*', required=False,
-                              help='(not implemeted) specs that should '
-                              'be added (omit all others)')
+    select.add_argument('-w',
+                        '--whitelist', nargs='*', required=False,
+                        help='(not implemeted) specs that should '
+                        'be added (omit all others)')
 
-    select_group.add_argument('-b',
-                              '--blacklist', nargs='*', required=False,
-                              help='(not implemented) specs that should '
-                              'be omitted (add all others)')
+    select.add_argument('-b',
+                        '--blacklist', nargs='*', required=False,
+                        help='(not implemented) specs that should '
+                        'be omitted (add all others)')
 
-    external_prsr.add_argument('path',
-                               help='The location of the external install '
-                               'directory')
+    ext.add_argument('path',
+                     help='The location of the external install '
+                     'directory')
+    ext.set_defaults(merge=False)
 
     command_dict['external'] = external
