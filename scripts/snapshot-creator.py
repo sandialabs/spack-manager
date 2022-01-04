1543551116ab811eb6af393dfd9680eda3db4c65
psakiev <psakiev@sandia.gov>
2022-01-03 22:19:08 -0700

    Add sha getter

--- scripts/snapshot-creator.py
+++ scripts/snapshot-creator.py
@@ -12,8 +12,11 @@ from manager_cmds.find_machine import find_machine
 import spack.environment as ev
 import spack.main
 import spack.util.spack_yaml as syaml
+import spack.util.executable
 
 from datetime import date
+
+git = spack.util.executable.which('git')
 
 arch = spack.main.SpackCommand('arch')
 manager = spack.main.SpackCommand('manager')
@@ -44,14 +47,17 @@ machine_specs = {
 
 def parse(stream):
     parser = argparse.ArgumentParser(
-        'create a timestamped snapshot for a Exawind machines')
+        'create a timestamped snapshot for registered machines')
     parser.add_argument(
         '-m', '--modules', action='store_true',
-        help="create modules to associate with each view in the environment")
+        help='create modules to associate with each view in the environment')
+    phases = ['create_env', 'develop', 'concretize', 'install']
     parser.add_argument(
-        '--just-setup', action='store_true',
-        help='only create the environment, don\'t concretize or install')
-    parser.set_defaults(modules=False, just_setup=False)
+        '--stop_after', '-sa', choices=phases,
+        help='stop script after this phase')
+    parser.add_argument('--use_develop', '-ud', action='store_true',
+                        help='use develop specs for roots and their immediate dependencies')
+    parser.set_defaults(modules=False, use_develop=False, stop_after='install')
 
     return parser.parse_args(stream)
 
@@ -94,26 +100,62 @@ def add_spec(env, extension, data, create_modules):
         syaml.dump(yaml, stream=f, default_flow_style=False)
 
 
-def add_develop_specs(env, develop_blacklist=['cmake', 'yaml-cpp']):
+def get_top_level_specs(env, blacklist=['cmake', 'yaml-cpp']):
+    env.concretize()
+    top_specs = set()
+    for root in env.roots():
+        if root.name in blacklist:
+            continue
+        top_specs.add(root.format('{name}{@version}'))
+        for dep in root.dependencies():
+            if dep.name not in blacklist:
+                top_specs.add(dep.format('{name}{@version}'))
+    return top_specs
+
+
+def find_latest_git_hash(env, spec_name):
+    spec = env.matching_spec(spec_name)
+    version_dict = spec.package_class.version[spec.version]
+    keys = version_dict.keys()
+
+    if 'branch' in keys:
+        # git branch
+        ref = 'refs/heads/%s' % version_dict['branch']
+    elif 'tag' in keys:
+        ref = 'refs/tags/%s' % version_dict['tag']
+    elif 'sha256' in keys:
+        # already matched
+        return version_dict['sha256']
+
+    # get all the entries and shas for github
+    query = git('ls-remote', spec.package.git, ref,
+                output=str, error=str).strip().split('\n')
+    assert len(query) == 1
+
+    sha, _ = query[0].split('\t')
+    return sha
+
+
+def add_develop_specs(env):
     # we have to concretize to solve the dependency tree to extract
     # the top level dependencies and make them develop specs.
     # anything that is not a develop spec is not gauranteed to get installed
     # since spack can reuse them for matching hashes
 
     print('Setting up develop specs')
-    env.concretize()
-    dev_specs = set()
-    for root in env.roots():
-        dev_specs.add(root.format('{name}{@version}'))
-        for dep in root.dependencies():
-            if dep.name not in develop_blacklist:
-                dev_specs.add(dep.format('{name}{@version}'))
+    dev_specs = get_top_level_specs(env)
 
     print(dev_specs, len(dev_specs))
     ev.activate(env)
     for spec_string in dev_specs:
         print('spack manager develop ' + spec_string)
-        manager('develop', spec_string)
+        # special treatment for trilinos since if fails with standard spack develop
+        if 'trilinos' in spec_string:
+            branch = spec_string.split('@')[-1]
+            manager('develop', '-rb',
+                    'https://github.com/trilinos/trilinos', branch, spec_string)
+        else:
+            manager('develop', spec_string)
     ev.deactivate()
 
 
@@ -124,29 +166,36 @@ def create_snapshots(args):
         os.environ['SPACK_MANAGER'], 'environments', extension)
 
     print('Creating snapshot environment')
-    manager('create-env', '-d', env_path)
+    # we add cmake so it is a root spec that will get added to the view
+    # so people using the snapshot don't have to rebuild
+    manager('create-env', '-d', env_path, '-s', 'cmake')
     e = ev.Environment(env_path)
     with e.write_transaction():
         e.yaml['spack']['concretization'] = 'separately'
         e.write()
 
-    # update the spack.yaml in memory so we down't have to carry
-    # unnecessary templates for each machine
-
     spec_data = machine_specs[machine]
 
     for s in spec_data:
         add_spec(e, extension, s, args.modules)
+    if args.stop_after == 'create_env':
+        return
 
-    add_develop_specs(e)
-    if args.just_setup:
+    if args.use_develop:
+        add_develop_specs(e)
+
+    if args.stop_after == 'develop':
         return
 
     ev.activate(e)
-    #concrete_specs = e.concretize(force=True)
-    #ev.display_specs(concrete_specs)
-    concertize('-f')
-    install()
+    print('Concretize')
+    concrete_specs = e.concretize(force=True)
+    ev.display_specs(concrete_specs)
+    # concertize('-f')
+    if args.stop_after == 'concretize':
+        return
+    print('Install')
+    install('-v')
 
 
 if __name__ == '__main__':
