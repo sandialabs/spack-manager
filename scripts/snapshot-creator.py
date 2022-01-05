f9b199cd96380a0451dd8e3076bb9f90f053d762
psakiev <psakiev@sandia.gov>
2022-01-05 00:20:12 -0700

    Clean up and add module creation

--- scripts/snapshot-creator.py
+++ scripts/snapshot-creator.py
@@ -6,6 +6,7 @@ and any associated modules
 
 import argparse
 import os
+import re
 import sys
 from manager_cmds.find_machine import find_machine
 
@@ -13,6 +14,7 @@ import spack.environment as ev
 import spack.main
 import spack.util.spack_yaml as syaml
 import spack.util.executable
+from spack.version import VersionList
 
 from datetime import date
 
@@ -21,22 +23,22 @@ git = spack.util.executable.which('git')
 arch = spack.main.SpackCommand('arch')
 manager = spack.main.SpackCommand('manager')
 add = spack.main.SpackCommand('add')
-uninstall = spack.main.SpackCommand('uninstall')
-concertize = spack.main.SpackCommand('concretize')
 install = spack.main.SpackCommand('install')
+module = spack.main.SpackCommand('module')
 
 base_spec = 'exawind+hypre+openfast'
 
 
 class SnapshotSpec:
-    def __init__(self, id='host-only', spec=base_spec):
+    def __init__(self, id='default', spec=base_spec):
         self.id = id
         self.spec = spec
 
 
 # a list of specs to build in the snapshot, 1 view will be created for each
 machine_specs = {
-    'darwin': [SnapshotSpec(spec='exawind ^nccmp'), SnapshotSpec(id='default')],
+    'darwin': [SnapshotSpec()],
+    'snl-hpc': [SnapshotSpec()],
     'ascicgpu': [SnapshotSpec(),
                  SnapshotSpec(
                      id='cuda',
@@ -51,20 +53,22 @@ def parse(stream):
     parser.add_argument(
         '-m', '--modules', action='store_true',
         help='create modules to associate with each view in the environment')
-    phases = ['create_env', 'develop', 'concretize', 'install']
+    phases = ['create_env', 'mod_specs', 'concretize', 'install']
     parser.add_argument(
         '--stop_after', '-sa', choices=phases,
         help='stop script after this phase')
     parser.add_argument('--use_develop', '-ud', action='store_true',
                         help='use develop specs for roots and their immediate dependencies')
+    parser.add_argument('--name', '-n', required=False,
+                        help='naem the environment something other than the date')
     parser.set_defaults(modules=False, use_develop=False, stop_after='install')
 
     return parser.parse_args(stream)
 
 
-def path_extension():
+def path_extension(name):
     return "exawind/snapshots/{arch}/{date}".format(
-        date=date.today().strftime("%Y%m%d"),
+        date=name if name else date.today().strftime("%Y%m%d"),
         arch=arch('-t').strip())
 
 
@@ -96,6 +100,25 @@ def add_spec(env, extension, data, create_modules):
     except AttributeError:
         yaml['spack']['view'] = view_dict
 
+    if create_modules:
+        module_path = os.path.join(
+            os.environ['SPACK_MANAGER'], 'modules')
+        module_dict = {data.id: {
+            'enable': ['tcl'],
+            'use_view': data.id,
+            'prefix_inspections': {'bin': ['PATH']},
+            'roots': {'tcl': module_path},
+            'arch_folder': False,
+            'tcl': {'projections': {'all': '%s/{name}-%s/{hash:4}' % (extension, data.id)},
+                    'hash_length': 0,
+                    'blacklist_implicits': True,
+                    'blacklist': ['cmake']}
+        }}
+        try:
+            yaml['spack']['modules'].update(module_dict)
+        except KeyError:
+            yaml['spack']['modules'] = module_dict
+
     with open(env.manifest_path, 'w') as f:
         syaml.dump(yaml, stream=f, default_flow_style=False)
 
@@ -106,19 +129,17 @@ def get_top_level_specs(env, blacklist=['cmake', 'yaml-cpp']):
     for root in env.roots():
         if root.name in blacklist:
             continue
-        top_specs.append(root)  # .format('{name}{@version}'))
+        top_specs.append(root)
         for dep in root.dependencies():
             if dep.name not in blacklist:
-                top_specs.append(dep)  # .format('{name}{@version}'))
+                top_specs.append(dep)
     # remove any duplicates
     top_specs = list(dict.fromkeys(top_specs))
     print('Top Level Specs:', [s.name for s in top_specs])
     return top_specs
 
 
-def find_latest_git_hash(env, spec_name, assign=False):
-    print(spec_name)
-    spec = env.matching_spec(spec_name)
+def find_latest_git_hash(spec):
     version_dict = spec.package_class.versions[spec.version]
     keys = version_dict.keys()
 
@@ -129,29 +150,70 @@ def find_latest_git_hash(env, spec_name, assign=False):
         ref = 'refs/tags/%s' % version_dict['tag']
     elif 'sha256' in keys:
         # already matched
-        return version_dict['sha256']
+        return None
+    else:
+        raise Exception('no known git type for ' +
+                        spec.format('//{hash} ({name}{@version})'))
 
-    # get all the entries and shas for github
+    # get the matching entry and shas for github
     query = git('ls-remote', spec.package.git, ref,
                 output=str, error=str).strip().split('\n')
     assert len(query) == 1
 
     sha, _ = query[0].split('\t')
+
     return sha
 
 
-def overwrite_yaml_with_full_spec_and_git_hashes():
-    pass
-    #new = spec.format('{name}@')+ hash + spec.format('{variants}')
+def replace_versions_with_hashes(spec_string, hash_dict):
+    specs = str(spec_string).strip().split(' ^')
+    new_specs = []
+    for spec in specs:
+        base, rest = spec.split('%')
+        name, version = base.split('@')
+        hash = hash_dict.get(name)
+        if hash:
+            version = hash
+        new_specs.append('{n}@{v}%{r}'.format(n=name,
+                                              v=version, r=rest))
+    final = ' ^'.join(new_specs)
+    assert '\n' not in final
+    return final
 
 
-def add_develop_specs(env, dev_specs):
+def use_latest_git_hashes(env, top_specs, blacklist=['cmake', 'yaml-cpp']):
+    with open(env.manifest_path, 'r') as f:
+        yaml = syaml.load(f)
+
+    roots = list(env.roots())
+
+    for i in range(len(roots)):
+        if roots[i].name not in blacklist:
+            hash_dict = {}
+            hash_dict[roots[i].name] = find_latest_git_hash(roots[i])
+
+            for dep in roots[i].dependencies():
+                if dep.name not in blacklist:
+                    hash_dict[dep.name] = find_latest_git_hash(dep)
+
+            yaml['spack']['specs'][i] = replace_versions_with_hashes(
+                roots[i].build_spec, hash_dict)
+
+    with open(env.manifest_path, 'w') as fout:
+        syaml.dump_config(yaml, stream=fout,
+                          default_flow_style=False)
+    env._re_read()
+
+
+def use_develop_specs(env, specs):
     # we have to concretize to solve the dependency tree to extract
     # the top level dependencies and make them develop specs.
     # anything that is not a develop spec is not gauranteed to get installed
     # since spack can reuse them for matching hashes
 
     print('Setting up develop specs')
+    dev_specs = list(dict.fromkeys(
+        [s.format('{name}{@version}') for s in specs]))
 
     ev.activate(env)
     for spec_string in dev_specs:
@@ -168,7 +230,7 @@ def add_develop_specs(env, dev_specs):
 
 def create_snapshots(args):
     machine = find_machine(verbose=False)
-    extension = path_extension()
+    extension = path_extension(args.name)
     env_path = os.path.join(
         os.environ['SPACK_MANAGER'], 'environments', extension)
 
@@ -187,28 +249,29 @@ def create_snapshots(args):
         add_spec(e, extension, s, args.modules)
 
     top_specs = get_top_level_specs(e)
-    for s in top_specs:
-        h = find_latest_git_hash(e, s)
-        print(s.name, h)
 
     if args.stop_after == 'create_env':
         return
 
     if args.use_develop:
-        add_develop_specs(e, top_specs)
+        use_develop_specs(e, top_specs)
+    else:
+        use_latest_git_hashes(e, top_specs)
 
-    if args.stop_after == 'develop':
+    if args.stop_after == 'mod_specs':
         return
 
     ev.activate(e)
     print('Concretize')
     concrete_specs = e.concretize(force=True)
     ev.display_specs(concrete_specs)
-    # concertize('-f')
     if args.stop_after == 'concretize':
         return
     print('Install')
     install('-v')
+    if args.modules:
+        print('Generate module files')
+        module('tcl', 'refresh', '-y')
 
 
 if __name__ == '__main__':
