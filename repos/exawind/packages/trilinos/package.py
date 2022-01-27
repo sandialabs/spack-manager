bedc0dc99c0fade842b643abc502e50ecaef7606
Jon Rood <jon.rood@nrel.gov>
2022-01-27 15:39:26 -0700

    Add nvcc_wrapper to repo and point trilinos to use it. Update example developer workflow script.

--- repos/exawind/packages/trilinos/package.py
+++ repos/exawind/packages/trilinos/package.py
@@ -1,11 +1,19 @@
 from spack import *
 from spack.pkg.builtin.trilinos import Trilinos as bTrilinos
+import os
 
 class Trilinos(bTrilinos):
     variant('stk_unit_tests', default=False,
             description='turn on STK unit tests')
 
     patch('kokkos.patch', when='+cuda')
+
+    def setup_dependent_package(self, module, dependent_spec):
+        if '+wrapper' in self.spec:
+            # Hardcode nvcc_wrapper path to avoid kokkos-nvcc-wrapper error with trilinos as an external
+            self.spec.kokkos_cxx = os.path.join(os.getenv('SPACK_MANAGER'), 'bin', 'nvcc_wrapper')
+        else:
+            self.spec.kokkos_cxx = spack_cxx
 
     def cmake_args(self):
         spec = self.spec
