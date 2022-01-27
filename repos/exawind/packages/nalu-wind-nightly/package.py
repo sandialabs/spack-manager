ba5f6ebcf98060fe91124039724d24ad91f3b184
Jon Rood <jon.rood@nrel.gov>
2022-01-27 09:54:22 -0700

    Fix cmake warning handling.

--- repos/exawind/packages/nalu-wind-nightly/package.py
+++ repos/exawind/packages/nalu-wind-nightly/package.py
@@ -62,8 +62,8 @@ class NaluWindNightly(bNaluWind, CudaPackage):
         cmake_options += self.cmake_args()
         cmake_options.remove('-G')
         cmake_options.remove('Unix Makefiles') # The space causes problems for ctest
-        #if '%intel' in spec:
-        #    cmake_options.remove('-DBoost_NO_BOOST_CMAKE=ON') # Avoid dashboard warning
+        if '%intel' in spec and '-DBoost_NO_BOOST_CMAKE=ON' in cmake_options:
+            cmake_options.remove('-DBoost_NO_BOOST_CMAKE=ON') # Avoid dashboard warning
         if machine == 'eagle.hpc.nrel.gov':
             cmake_options.append(define('TEST_ABS_TOL', '1e-10'))
             cmake_options.append(define('TEST_REL_TOL', '1e-8'))
