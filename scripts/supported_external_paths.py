8d77cbb5dec62f9fcc755cb03f99c5151ea42b09
psakievich <psakiev@sandia.gov>
2022-02-01 14:35:56 -0700

    Add auto assignment of env variable for external detections (#138)

---
+++ scripts/supported_external_paths.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env spack-python
+import manager_cmds.find_machine as find_machine
+
+
+def get_path_to_externals():
+    machine = find_machine.find_machine(verbose=False)
+    if machine in ['cee', 'snl-hpc', 'ascicgpu']:
+        return '/projects/wind/spack-manager'
+    else:
+        return
+
+
+if __name__ == '__main__':
+    path = get_path_to_externals()
+    # echo path if it exists
+    if path:
+        print(path)
