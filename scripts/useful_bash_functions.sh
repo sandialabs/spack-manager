9f71a575acefe5420e8a404d1e3b5b6469a32bc5
Jon Rood <jon.rood@nrel.gov>
2022-02-01 11:19:09 -0700

    Remove signal trapping from quick-develop. (#136)

--- scripts/useful_bash_functions.sh
+++ scripts/useful_bash_functions.sh
@@ -1,16 +1,21 @@
+#!/bin/bash
+
 cmd() {
   echo "+ $@"
   eval "$@"
 }
+
 # function to initialize spack-manager's spack instance
 function spack-start() {
   source ${SPACK_MANAGER}/start.sh
 }
+
 # function to quickly activate an environment
 function quick-activate() {
   cmd "spack-start"
   cmd "spack env activate -p -d $1"
 }
+
 # convenience function for quickly creating an environment
 # and activating it in the current shell
 function quick-create() {
@@ -26,11 +31,9 @@ function quick-create() {
   EPATH=$(cat $SPACK_MANAGER/.tmp/created_env_path.txt)
   cmd "spack env activate --dir ${EPATH} --prompt"
 }
+
 # function to create, activate, concretize and attempt to install a develop environment all in one step
 function quick-develop() {
-  # Trap and kill background processes
-  trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-
   # since we want this to run in the active shell
   # we mush manually return instead of exiting with set -e
 
