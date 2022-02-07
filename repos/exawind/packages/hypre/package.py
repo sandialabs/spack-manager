92f7b504aa3dcb1332a3a60d47ab103c0fa0bb32
Jon Rood <jon.rood@nrel.gov>
2022-02-07 14:07:09 -0700

    Add clean phase to hypre (#152)
    * Add clean phase to hypre.
    
    * Change order.

--- repos/exawind/packages/hypre/package.py
+++ repos/exawind/packages/hypre/package.py
@@ -3,6 +3,13 @@ from spack.pkg.builtin.hypre import Hypre as bHypre
 import os
 
 class Hypre(bHypre):
+
+    phases = ['autoreconf', 'configure', 'clean', 'build', 'install']
+
+    def clean(self, spec, prefix):
+        with working_dir('src'):
+            make('clean')
+
     def _configure_args(self):
         spec = self.spec
         options = super(Hypre, self)._configure_args()                        
