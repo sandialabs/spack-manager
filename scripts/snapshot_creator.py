b27a49e98f847073ec8fdee42fef7954de720129
psakiev <psakiev@sandia.gov>
2022-02-07 12:33:23 -0700

    flake8

--- scripts/snapshot_creator.py
+++ scripts/snapshot_creator.py
@@ -89,9 +89,9 @@ machine_specs = {
                             '+cuda+amr_wind_gpu+nalu_wind_gpu '
                             'cuda_arch=70 %gcc')],
     'snl-hpc': [SnapshotSpec()],
-    'ascicgpu': [SnapshotSpec(
-                     'gcc-cuda', base_spec + '+cuda+amr_wind_gpu+nalu_wind_gpu'
-                     ' cuda_arch=70'), SnapshotSpec(id='gcc'), ],
+    'ascicgpu': [SnapshotSpec('gcc-cuda', base_spec + '+cuda+amr_wind_gpu'
+                              '+nalu_wind_gpu cuda_arch=70'),
+                 SnapshotSpec(id='gcc'), ],
 }
 
 
