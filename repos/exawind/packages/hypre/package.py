c6a246aeb91bed34d39a10e223a177d9f49c4033
Jon Rood <jon.rood@nrel.gov>
2022-02-08 10:30:14 -0700

    Remove clean phase from hypre. (#156)

--- repos/exawind/packages/hypre/package.py
+++ repos/exawind/packages/hypre/package.py
@@ -3,13 +3,6 @@ from spack.pkg.builtin.hypre import Hypre as bHypre
 import os
 
 class Hypre(bHypre):
-
-    phases = ['autoreconf', 'configure', 'clean', 'build', 'install']
-
-    def clean(self, spec, prefix):
-        with working_dir('src'):
-            make('clean')
-
     def _configure_args(self):
         spec = self.spec
         options = super(Hypre, self)._configure_args()                        
