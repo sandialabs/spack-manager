5c5953abbd3e8177bedc5ac3cd1c31cac5199f85
psakievich <psakiev@sandia.gov>
2022-02-08 08:01:25 -0700

    ASCICGPU: Config update (#150)
    * ASCICGPU: Config update
    
    Update ascicgpu to use compilers matching eagle
    
    * Update template to match
    
    * Move file to allow --show-cores to work
    
    * Update configs again
    
    * Remove ascicgpu from darwin test in ci
    
    * flake fixes attempt
    
    * flake8
    
    * Restrict darwin testing
    
    * flake

--- start.sh
+++ start.sh
@@ -19,7 +19,7 @@ fi
 export SPACK_ROOT=${SPACK_MANAGER}/spack
 export SPACK_DISABLE_LOCAL_CONFIG=true
 export SPACK_USER_CACHE_PATH=${SPACK_MANAGER}/.cache
-export PYTHONPATH=${PYTHONPATH}:${SPACK_MANAGER}/scripts:${SPACK_MANAGER}/spack-scripting/scripting/cmd
+export PYTHONPATH=${PYTHONPATH}:${SPACK_MANAGER}/scripts:${SPACK_MANAGER}/spack-scripting/scripting/cmd:${SPACK_MANAGER}/spack-scripting/scripting
 source ${SPACK_ROOT}/share/spack/setup-env.sh
 
 # Clean Spack misc caches
