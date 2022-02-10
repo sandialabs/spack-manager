1b53cc14905e463e15bab1f678956057dcf2d548
Jon Rood <jon.rood@nrel.gov>
2022-02-10 09:54:17 -0700

    Allow for sourcing start.sh multiple times.

--- start.sh
+++ start.sh
@@ -23,30 +23,30 @@ function spack-start() {
   ########################################################
   # Environment stuff
   ########################################################
-  export SPACK_ROOT=${SPACK_MANAGER}/spack
-  export SPACK_DISABLE_LOCAL_CONFIG=true
-  export SPACK_USER_CACHE_PATH=${SPACK_MANAGER}/.cache
   if [[ -z ${SPACK_MANAGER_MACHINE} ]]; then
+    export SPACK_ROOT=${SPACK_MANAGER}/spack
+    export SPACK_DISABLE_LOCAL_CONFIG=true
+    export SPACK_USER_CACHE_PATH=${SPACK_MANAGER}/.cache
     export PYTHONPATH=${PYTHONPATH}:${SPACK_MANAGER}/scripts:${SPACK_MANAGER}/spack-scripting/scripting/cmd:${SPACK_MANAGER}/spack-scripting/scripting
     source ${SPACK_ROOT}/share/spack/setup-env.sh
-  fi
   
-  # Clean Spack misc caches
-  # Put this back in if outdated caches directory still causes problems when updating Spack submodule
-  #spack clean -m
-  
-  if [[ -z $(spack config --scope site blame config | grep spack-scripting) ]]; then
-      spack config --scope site add "config:extensions:[${SPACK_MANAGER}/spack-scripting]"
+    # Clean Spack misc caches
+    # Put this back in if outdated caches directory still causes problems when updating Spack submodule
+    #spack clean -m
+    
+    if [[ -z $(spack config --scope site blame config | grep spack-scripting) ]]; then
+        spack config --scope site add "config:extensions:[${SPACK_MANAGER}/spack-scripting]"
+    fi
+    
+    export SPACK_MANAGER_MACHINE=$(spack manager find-machine)
+    if [[ "${SPACK_MANAGER_MACHINE}" == "NOT-FOUND" ]]; then
+        echo "Machine not found."
+    fi
+    if [[ -n "$(${SPACK_MANAGER}/scripts/supported_external_paths.py)" ]]; then
+        export SPACK_MANAGER_EXTERNAL=$(${SPACK_MANAGER}/scripts/supported_external_paths.py)
+    fi
+    export PATH=${PATH}:${SPACK_MANAGER}/scripts
   fi
-  
-  export SPACK_MANAGER_MACHINE=$(spack manager find-machine)
-  if [[ "${SPACK_MANAGER_MACHINE}" == "NOT-FOUND" ]]; then
-      echo "Machine not found."
-  fi
-  if [[ -n "$(${SPACK_MANAGER}/scripts/supported_external_paths.py)" ]]; then
-      export SPACK_MANAGER_EXTERNAL=$(${SPACK_MANAGER}/scripts/supported_external_paths.py)
-  fi
-  export PATH=${PATH}:${SPACK_MANAGER}/scripts
 }
 
 # function to quickly activate an environment
