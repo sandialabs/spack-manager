b2f4797f1015d344ba6bdd2ed779ecd3aa6ba0b1
psakievich <psakiev@sandia.gov>
2022-02-16 09:41:07 -0700

    Update Spack to capture UVM variant for Trilinos (#186)
    * Update Spack to turn off cuda UVM with trilinos
    
    * Add new UVM flag from spack
    
    * Fix concretization CI
    
    * More CI fixes, and doc formatting
    
    * Style and matrix CI fixes
    
    * More doc style stuff
    
    * Bold not italic
    
    * Capitalization

--- tests/ci_config_concretization.py
+++ tests/ci_config_concretization.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env spack-python
 from tempfile import TemporaryDirectory
-import manager_cmds.find_machine as find_machine
+from manager_cmds.find_machine import find_machine as find_machine
 from manager_cmds.find_machine import machine_list
 import spack.environment as env
 import spack.main
@@ -26,19 +26,22 @@ def check_config_concretizations(name, ref_yaml):
 def run_tests(args):
     failure = False
     this_machine = find_machine(verbose=False)
-    # only test darwin on matching ci platform
-    if this_machine == 'darwin':
-        machine_names = ['darwin']
+    machine_names = list(machine_list.keys())
+    matrix_test_machines = ['eagle', 'summit']
+    if '_matrix.yaml' in args.yaml:
+        machine_names = matrix_test_machines
     else:
-        machine_names = list(machine_list.keys())
-        machine_names.remove('darwin')
-        matrix_test_machines = ['eagle', 'summit']
-
-        if '_matrix.yaml' in args.yaml:
-            machine_names = matrix_test_machines
+        machine_names = list(
+            set(machine_names) - set(matrix_test_machines))
+    # only test darwin on matching ci platform
+    # and return early on darwin for matrix test
+    if this_machine == 'darwin':
+        if 'darwin' in machine_names:
+            machine_names = ['darwin']
         else:
-            machine_names = list(
-                set(machine_names) - set(matrix_test_machines))
+            return
+    else:
+        machine_names.remove('darwin')
 
     for name in machine_names:
         try:
