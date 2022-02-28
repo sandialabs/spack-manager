39fe0909f3a9453e3c9384ad69e487161034849c
Jon Rood <jon.rood@nrel.gov>
2022-02-28 14:54:40 -0500

    Undefine HIP device compile macro for exawind driver when nalu-wind is not ROCm.

--- repos/exawind/packages/exawind/package.py
+++ repos/exawind/packages/exawind/package.py
@@ -84,6 +84,8 @@ class Exawind(CMakePackage, CudaPackage, ROCmPackage):
     def setup_build_environment(self, env):
         if '+asan' in self.spec:
             env.append_flags("CXXFLAGS", "-fsanitize=address -fno-omit-frame-pointer -fsanitize-blacklist={0}".format(join_path(self.package_dir, 'sup.asan')))
+        if '+rocm+amr_wind_gpu~nalu_wind_gpu' in self.spec:
+            env.append_flags("CXXFLAGS", "-U__HIP_DEVICE_COMPILE__")
 
     @run_after('cmake')
     def copy_compile_commands(self):
