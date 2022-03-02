7fed319379024c2d9940e90ed251073aec96822c
psakiev <psakiev@sandia.gov>
2022-03-02 16:03:00 -0700

    Fix bug in find_machine

--- spack-scripting/tests/test_find_machine.py
+++ spack-scripting/tests/test_find_machine.py
@@ -1,2 +1,10 @@
-def test_find_machine():
-    pass
+from tabnanny import verbose
+from manager_cmds.find_machine import *
+from unittest.mock import patch
+
+
+@patch('manager_cmds.find_machine.socket.gethostname',
+       return_value='abcdefghijklmnopqrstuvwxyz1234567890'
+       'ABCDEFGHILJKLMNOPQRSTUVWXYZ')
+def test_find_machine_doesnt_return_arbitrary_true(mock_socket):
+    assert find_machine(verbose=False) == 'NOT-FOUND'
