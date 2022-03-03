60d71c6dbe13261214c53b0f463a6d9024639d69
psakiev <psakiev@sandia.gov>
2022-03-02 17:30:32 -0700

    Stylz

--- spack-scripting/tests/test_find_machine.py
+++ spack-scripting/tests/test_find_machine.py
@@ -1,10 +1,9 @@
-from manager_cmds.find_machine import *
-from unittest.mock import patch
 import pytest
+from manager_cmds.find_machine import *
 
 
 @pytest.mark.parametrize("test_function", [is_cee, is_snl_hpc, is_jlse])
 def test_functional_checks_dont_return_false_positives(test_function):
-    assert test_function('abcdefghijklmnopqrstuvwxyz') == False
-    assert test_function('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == False
-    assert test_function('0123456789') == False
+    assert test_function('abcdefghijklmnopqrstuvwxyz') is False
+    assert test_function('ABCDEFGHIJKLMNOPQRSTUVWXYZ') is False
+    assert test_function('0123456789') is False
