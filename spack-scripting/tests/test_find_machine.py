f9fed513823a908bb6d236ed008c90cc289a6718
psakiev <psakiev@sandia.gov>
2022-03-02 17:23:00 -0700

    Add tests

--- spack-scripting/tests/test_find_machine.py
+++ spack-scripting/tests/test_find_machine.py
@@ -1,10 +1,10 @@
-from tabnanny import verbose
 from manager_cmds.find_machine import *
 from unittest.mock import patch
+import pytest
 
 
-@patch('manager_cmds.find_machine.socket.gethostname',
-       return_value='abcdefghijklmnopqrstuvwxyz1234567890'
-       'ABCDEFGHILJKLMNOPQRSTUVWXYZ')
-def test_find_machine_doesnt_return_arbitrary_true(mock_socket):
-    assert find_machine(verbose=False) == 'NOT-FOUND'
+@pytest.mark.parametrize("test_function", [is_cee, is_snl_hpc, is_jlse])
+def test_functional_checks_dont_return_false_positives(test_function):
+    assert test_function('abcdefghijklmnopqrstuvwxyz') == False
+    assert test_function('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == False
+    assert test_function('0123456789') == False
