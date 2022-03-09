16f79633189b821bd2be81a27638b144e28fb1a2
psakiev <psakiev@sandia.gov>
2022-03-08 21:42:37 -0700

    Remove arch from externals

--- spack-scripting/scripting/cmd/manager_cmds/external.py
+++ spack-scripting/scripting/cmd/manager_cmds/external.py
@@ -98,7 +98,7 @@ def write_spec(view, spec):
     return template.format(
         name=spec.name,
         short_spec=spec.format(
-            '{name}{@version}{%compiler}{variants}{arch=architecture}'),
+            '{name}{@version}{%compiler}{variants}'),
         prefix=view.get_projection_for_spec(spec))
 
 
