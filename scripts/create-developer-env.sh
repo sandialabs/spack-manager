1a7ebdfefad4af2167247038da1137a97441012b
Jon Rood <jon.rood@nrel.gov>
2022-03-16 14:16:44 -0600

    Update example create dev env script (#247)
    * Update create dev env script.
    
    Co-authored-by: psakievich <psakiev@sandia.gov>

--- scripts/create-developer-env.sh
+++ scripts/create-developer-env.sh
@@ -23,30 +23,19 @@ cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
 
 printf "\nCreating developer environment...\n"
 if [ "${SPACK_MANAGER_MACHINE}" == 'eagle' ]; then
-  SNAPSHOT_DIR='/projects/exawind/exawind-snapshots/environment-latest'
   COMPILER='gcc'
   SPEC="nalu-wind@master+hypre+cuda cuda_arch=70 %${COMPILER}"
-  BLACKLIST='nalu-wind hypre'
   DEV_COMMAND='spack manager develop nalu-wind@master; spack manager develop hypre@develop'
-  VIEW="${COMPILER}-cuda"
 elif [ "${SPACK_MANAGER_MACHINE}" == 'rhodes' ]; then
-  SNAPSHOT_DIR='/projects/ecp/exawind/exawind-snapshots/environment-latest'
   COMPILER='gcc'
   SPEC="nalu-wind@master+hypre %${COMPILER}"
-  BLACKLIST='nalu-wind hypre'
   DEV_COMMAND='spack manager develop nalu-wind@master; spack manager develop hypre@develop'
-  VIEW="${COMPILER}"
 fi
-# all of these commands will eventually be combined into
-# quick-develop --name --exawind --spec $SPEC hypre@develop
-# still missing view selection for this
 
-# updated to use the new --name flag
 cmd "spack manager create-env --name exawind --spec '${SPEC}'"
 cmd "spack env activate -d ${SPACK_MANAGER}/environments/exawind"
-# if you do develop first then it will now auto-blacklist those specs for you
-cmd "spack manager external ${SNAPSHOT_DIR} -v ${VIEW} --blacklist ${BLACKLIST}"
 cmd "${DEV_COMMAND}"
+cmd "spack manager external --latest"
 cmd "spack concretize -f"
 cmd "spack install"
 
