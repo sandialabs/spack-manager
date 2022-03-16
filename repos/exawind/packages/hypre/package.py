360414269e0a4a829965d59f77e086caaacf3857
Jon Rood <jon.rood@nrel.gov>
2022-03-16 11:27:51 -0600

    Add distclean and clean phases to hypre (#246)
    * Add distclean and clean phases to hypre.
    
    * Update repos/exawind/packages/hypre/package.py
    
    Co-authored-by: psakievich <psakiev@sandia.gov>
    
    * Update repos/exawind/packages/hypre/package.py
    
    Co-authored-by: psakievich <psakiev@sandia.gov>
    
    Co-authored-by: psakievich <psakiev@sandia.gov>

--- repos/exawind/packages/hypre/package.py
+++ repos/exawind/packages/hypre/package.py
@@ -4,6 +4,18 @@ import os
 
 class Hypre(bHypre):
 
+    phases = ['autoreconf', 'distclean', 'configure', 'clean', 'build', 'install']
+
+    def distclean(self, spec, prefix):
+        with working_dir('src'):
+            if 'SPACK_MANAGER_CLEAN_HYPRE' in os.environ:
+                make('distclean')
+
+    def clean(self, spec, prefix):
+        with working_dir('src'):
+            if 'SPACK_MANAGER_CLEAN_HYPRE' in os.environ:
+                make('clean')
+
     def _configure_args(self):
         spec = self.spec
         options = super(Hypre, self)._configure_args()
