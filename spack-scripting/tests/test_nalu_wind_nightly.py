15514eaac0b3f49c5c4285baa8dc557551e5f89c
Timothy A. Smith <tasmit@sandia.gov>
2022-05-12 13:57:51 -0600

    wip creating a test file for the nalu-wind-nightly package

---
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -0,0 +1,5 @@
+import pytest
+import importlib
+
+nalu_wind_nightly = importlib.import_module('...repos.exawind.packages.nalu-wind-nightly.package', 'package')
+#from ...repos.exawind.packages.nalu-wind-nightly.package import *
