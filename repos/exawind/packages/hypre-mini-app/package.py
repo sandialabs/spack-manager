e6c2687501c6f965c091cffa0e55d3287566d9ed
psakievich <psakiev@sandia.gov>
2022-05-12 09:25:36 -0600

    Create and add license and copyright statements (#278)

--- repos/exawind/packages/hypre-mini-app/package.py
+++ repos/exawind/packages/hypre-mini-app/package.py
@@ -1,7 +1,9 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright (c) 2022, National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# This software is released under the BSD 3-clause license. See LICENSE file
+# for more details.
 
 from spack import *
 
