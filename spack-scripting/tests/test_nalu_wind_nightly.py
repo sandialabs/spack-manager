fc00cbedbd6a2b0ce5430fe632d687087fa0338c
psakiev <psakiev@sandia.gov>
2022-05-14 15:07:03 -0600

    Attempt to flush out test

--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 import spack.repo
@@ -5,5 +6,7 @@ from spack.spec import Spec
 
 
 def test_can_read_exawind_repo(exawind_repo_path):
+    assert os.path.isdir(exawind_repo_path)
     with spack.repo.use_repositories(exawind_repo_path) as exawind_repo:
         concrete_spec = Spec('nalu-wind-nightly').concretize()
+        assert concrete_spec.variants('extra_name')
