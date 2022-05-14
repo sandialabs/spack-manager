ed592358893bc95d29f721d93a82490020b4c43e
psakiev <psakiev@sandia.gov>
2022-05-14 15:03:17 -0600

    Attempting to concretize and failing

--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -6,4 +6,4 @@ from spack.spec import Spec
 
 def test_can_read_exawind_repo(exawind_repo_path):
     with spack.repo.use_repositories(exawind_repo_path) as exawind_repo:
-        spec = Spec('nalu-wind-nightly')
+        concrete_spec = Spec('nalu-wind-nightly').concretize()
