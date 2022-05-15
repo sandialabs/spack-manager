b82260a0c966b9fbb199e6190fc5b3daebbd04fd
Timothy A. Smith <tasmit@sandia.gov>
2022-05-14 21:04:07 -0600

    fixed the concretization issue, still fails though

--- spack-scripting/tests/conftest.py
+++ spack-scripting/tests/conftest.py
@@ -7,3 +7,7 @@ from spack.test.conftest import *  # noqa: F401
 @pytest.fixture(scope='session')
 def exawind_repo_path():
     yield os.path.join(os.environ['SPACK_MANAGER'], 'repos', 'exawind')
+
+@pytest.fixture(scope='session')
+def builtin_repo_path():
+    yield os.path.join(os.environ['SPACK_MANAGER'], 'spack', 'var', 'spack', 'repos', 'builtin')
--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -5,10 +5,12 @@ import spack.repo
 from spack.spec import Spec
 
 
-def test_can_read_exawind_repo(exawind_repo_path):
+def test_can_read_exawind_repo(exawind_repo_path, builtin_repo_path):
     assert os.path.isdir(exawind_repo_path)
-    with spack.repo.use_repositories(exawind_repo_path) as exawind_repo:
-        concrete_spec = Spec('nalu-wind-nightly').concretize()
+    assert os.path.isdir(builtin_repo_path)
+    with spack.repo.use_repositories(exawind_repo_path, builtin_repo_path) as exawind_repo:
+        abstract_spec = Spec('nalu-wind-nightly')
+        concrete_spec = abstract_spec.concretize()
         assert concrete_spec.variants('extra_name')
         package = concrete_spec.package
         package.ctest_args()
