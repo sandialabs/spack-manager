81abcd42e9753342a0b23f98617f0a4d1dbc7a47
Timothy A. Smith <tasmit@sandia.gov>
2022-05-14 22:47:11 -0600

    really hoping flake is happy now

--- spack-scripting/tests/conftest.py
+++ spack-scripting/tests/conftest.py
@@ -7,6 +7,7 @@
 
 
 import os
+
 import pytest
 
 import spack.paths
@@ -19,20 +20,20 @@ from spack.test.conftest import *  # noqa: F401
 def builtin_repo_path():
     yield spack.paths.packages_path
 
+
 @pytest.fixture(scope='session')
 def exawind_repo_path():
     yield os.path.join(os.environ['SPACK_MANAGER'], 'repos', 'exawind')
+
 
 @pytest.fixture(scope='session')
 def create_package(builtin_repo_path, exawind_repo_path):
     packages = {}
 
-
     def _create(spec):
         if spec not in packages:
             packages[spec] = _create_new(spec)
         return packages[spec]
-
 
     def _create_new(spec):
         with spack.repo.use_repositories(builtin_repo_path, exawind_repo_path):
