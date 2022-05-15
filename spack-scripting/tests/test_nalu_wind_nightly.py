372914dcb21c40f059dba02b87745f01d2d5ce26
Timothy A. Smith <tasmit@sandia.gov>
2022-05-14 20:46:56 -0600

    add code to run package functions

--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -10,3 +10,6 @@ def test_can_read_exawind_repo(exawind_repo_path):
     with spack.repo.use_repositories(exawind_repo_path) as exawind_repo:
         concrete_spec = Spec('nalu-wind-nightly').concretize()
         assert concrete_spec.variants('extra_name')
+        package = concrete_spec.package
+        package.ctest_args()
+        assert package.spec.variants['extra_name'] != 'default'
