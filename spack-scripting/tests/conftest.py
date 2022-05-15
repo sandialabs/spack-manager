7909ce3b881f13d604475e10b2cc22fbe1e06cc9
Timothy A. Smith <tasmit@sandia.gov>
2022-05-14 21:34:42 -0600

    move package creation to a fixture

--- spack-scripting/tests/conftest.py
+++ spack-scripting/tests/conftest.py
@@ -1,13 +1,25 @@
 import os
 import pytest
 
+import spack.repo
+from spack.spec import Spec
 from spack.test.conftest import *  # noqa: F401
 
+
+@pytest.fixture(scope='session')
+def builtin_repo_path():
+    yield os.path.join(os.environ['SPACK_MANAGER'], 'spack', 'var', 'spack', 'repos', 'builtin')
 
 @pytest.fixture(scope='session')
 def exawind_repo_path():
     yield os.path.join(os.environ['SPACK_MANAGER'], 'repos', 'exawind')
 
 @pytest.fixture(scope='session')
-def builtin_repo_path():
-    yield os.path.join(os.environ['SPACK_MANAGER'], 'spack', 'var', 'spack', 'repos', 'builtin')
+def create_package(builtin_repo_path, exawind_repo_path):
+    def create(spec):
+        with spack.repo.use_repositories(exawind_repo_path, builtin_repo_path) as exawind_repo:
+            spec = Spec('nalu-wind-nightly')
+            spec.concretize()
+            return spec.package
+
+    return create
--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -1,17 +1,8 @@
 import os
 import pytest
 
-import spack.repo
-from spack.spec import Spec
 
-
-def test_can_read_exawind_repo(exawind_repo_path, builtin_repo_path):
-    assert os.path.isdir(exawind_repo_path)
-    assert os.path.isdir(builtin_repo_path)
-    with spack.repo.use_repositories(exawind_repo_path, builtin_repo_path) as exawind_repo:
-        spec = Spec('nalu-wind-nightly')
-        spec.concretize()
-        assert spec.variants['extra_name'].value == 'default'
-        package = spec.package
-        #package.ctest_args()  # test chokes on setting CMAKE_CXX_COMPILER from mpi spec ...
-        assert package.spec.variants['extra_name'].value == 'default'
+def test_can_read_exawind_repo(create_package):
+    package = create_package('nalu-wind-nightly')
+    #package.ctest_args()  # test chokes on setting CMAKE_CXX_COMPILER from mpi spec ...
+    assert package.spec.variants['extra_name'].value == 'default'
