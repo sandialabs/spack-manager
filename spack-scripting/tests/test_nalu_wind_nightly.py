f6720d53e6301be97a47834fe6bc8cbb198bef6b
Timothy A. Smith <tasmit@sandia.gov>
2022-05-15 19:48:22 -0600

    delete tests that were only for verifying correct spack usage

--- spack-scripting/tests/test_nalu_wind_nightly.py
+++ spack-scripting/tests/test_nalu_wind_nightly.py
@@ -9,13 +9,3 @@
 def test_package_name(create_package):
     package = create_package('nalu-wind-nightly')
     assert package.name == 'nalu-wind-nightly'
-
-
-def test_package_picks_up_variants(create_package):
-    package = create_package('nalu-wind-nightly+snl')
-    assert package.spec.variants['snl'].value
-
-
-def test_package_picks_up_default_variants(create_package):
-    package = create_package('nalu-wind-nightly')
-    assert 'snl' not in package.spec.variants
