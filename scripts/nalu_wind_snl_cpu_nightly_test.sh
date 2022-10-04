56a22f8b7cb5b1bc8d4156e5fec14d9aa813e169
Timothy A. Smith <tasmit@sandia.gov>
2022-10-04 12:12:26 -0600

    add SNL nightly testing cronjob scripts

---
+++ scripts/nalu_wind_snl_cpu_nightly_test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+export SPACK_MANAGER=/projects/wind/wind-testing-cpu
+export SPACK_MANAGER_GOLDS_DIR=/projects/wind/golds-cpu
+cd $SPACK_MANAGER
+
+$SPACK_MANAGER/scripts/update-spack-manager-repo.sh &> $SPACK_MANAGER/logs/last-spack-manager-repo-update.txt
+$SPACK_MANAGER/scripts/run-exawind-nightly-tests.sh &> $SPACK_MANAGER/logs/last-exawind-test-script-invocation.txt
+
+chgrp -R wg-sierra-users $SPACK_MANAGER_GOLDS_DIR
+chmod -R o-rwx $SPACK_MANAGER_GOLDS_DIR
+chmod -R g+rwX $SPACK_MANAGER_GOLDS_DIR
+find $SPACK_MANAGER_GOLDS_DIR -type d | xargs chmod g+s
