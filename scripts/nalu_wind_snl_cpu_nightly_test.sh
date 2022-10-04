c8118c34a5ee4a3fc45978c9818f4b2f5b13b457
Timothy A. Smith <tasmit@sandia.gov>
2022-10-04 12:32:50 -0600

    fix permissions for gold and log files in SNL nightly testing scripts

--- scripts/nalu_wind_snl_cpu_nightly_test.sh
+++ scripts/nalu_wind_snl_cpu_nightly_test.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+function set_gold_perms() {
+  chgrp -R wg-sierra-users $1
+  chmod -R o-rwx $1
+  chmod -R g+rwX $1
+  find $1 -type d | xargs chmod g+s
+}
+
 export SPACK_MANAGER=/projects/wind/wind-testing-cpu
 export SPACK_MANAGER_GOLDS_DIR=/projects/wind/golds-cpu
 cd $SPACK_MANAGER
@@ -7,7 +14,9 @@ cd $SPACK_MANAGER
 $SPACK_MANAGER/scripts/update-spack-manager-repo.sh &> $SPACK_MANAGER/logs/last-spack-manager-repo-update.txt
 $SPACK_MANAGER/scripts/run-exawind-nightly-tests.sh &> $SPACK_MANAGER/logs/last-exawind-test-script-invocation.txt
 
-chgrp -R wg-sierra-users $SPACK_MANAGER_GOLDS_DIR
-chmod -R o-rwx $SPACK_MANAGER_GOLDS_DIR
-chmod -R g+rwX $SPACK_MANAGER_GOLDS_DIR
-find $SPACK_MANAGER_GOLDS_DIR -type d | xargs chmod g+s
+set_gold_perms ${SPACK_MANAGER_GOLDS_DIR}-stable
+set_gold_perms ${SPACK_MANAGER_GOLDS_DIR}-develop
+
+chgrp wg-sierra-users $SPACK_MANAGER
+chgrp -R wg-sierra-users $SPACK_MANAGER/logs
+chmod -R g+rx $SPACK_MANAGER/logs
