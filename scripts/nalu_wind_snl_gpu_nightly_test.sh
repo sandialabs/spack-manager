c3a403a0bbdcdb16e04710eb60d5d281c40de8b3
Timothy Smith <58484958+tasmith4@users.noreply.github.com>
2022-10-04 13:07:55 -0600

    SNL nightly scripts (#364)
    * add SNL nightly testing cronjob scripts
    
    * update documentation for SNL nightly testing
    
    * fix permissions for gold and log files in SNL nightly testing scripts

---
+++ scripts/nalu_wind_snl_gpu_nightly_test.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+function set_gold_perms() {
+  chgrp -R wg-sierra-users $1
+  chmod -R o-rwx $1
+  chmod -R g+rwX $1
+  find $1 -type d | xargs chmod g+s
+}
+
+export SPACK_MANAGER=/projects/wind/wind-testing-gpu
+export SPACK_MANAGER_GOLDS_DIR=/projects/wind/golds-gpu
+cd $SPACK_MANAGER
+
+$SPACK_MANAGER/scripts/update-spack-manager-repo.sh &> $SPACK_MANAGER/logs/last-spack-manager-repo-update.txt
+$SPACK_MANAGER/scripts/run-exawind-nightly-tests.sh &> $SPACK_MANAGER/logs/last-exawind-test-script-invocation.txt
+
+set_gold_perms ${SPACK_MANAGER_GOLDS_DIR}-stable
+set_gold_perms ${SPACK_MANAGER_GOLDS_DIR}-develop
+
+chgrp wg-sierra-users $SPACK_MANAGER
+chgrp -R wg-sierra-users $SPACK_MANAGER/logs
+chmod -R g+rx $SPACK_MANAGER/logs
