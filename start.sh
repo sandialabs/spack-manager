5cb78fd3177800df6acca1707dda5bc62682e8d0
Timothy A. Smith <tasmit@sandia.gov>
2022-10-04 09:28:00 -0600

    remove changes to sm-clean

--- start.sh
+++ start.sh
@@ -194,9 +194,9 @@ function sm-clean(){
     return
   fi
   echo "Remove user cache of configs:"
-  cmd "rm -rf ${SPACK_USER_CONFIG_PATH}"
+  cmd "rm -rf ~/.spack"
   echo "Remove Spack-Manager cache:"
-  cmd "rm -rf ${SPACK_USER_CACHE_PATH}"
+  cmd "rm -rf ${SPACK_MANAGER}/.cache"
   echo "Use the native Spack clean system":
   cmd "spack clean --all"
   echo "Cleaning out pycache from spack-manager repos:"
