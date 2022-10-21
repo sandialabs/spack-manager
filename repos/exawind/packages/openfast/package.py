5aa664d3d704923e01554f3678a95242ac2465c3
psakiev <psakiev@sandia.gov>
2022-10-21 13:46:58 -0600

    Revert "Update OpenFAST package to use latest OpenFAST package in Spack. (#377)"
    This reverts commit 388716fec44929087b6b0fdd1b0efb5ad4520071.

--- repos/exawind/packages/openfast/package.py
+++ repos/exawind/packages/openfast/package.py
@@ -8,5 +8,12 @@
 from spack.pkg.builtin.openfast import Openfast as bOpenfast
 
 class Openfast(bOpenfast):
+    variant('netcdf', default=False, description='Use netcdf')
+    depends_on('netcdf-c', when='+netcdf')
     patch('hub_seg_fault.patch')
     patch('segfault_message.patch', when='%clang@12.0.1 build_type=RelWithDebInfo')
+    # Avoid using HDF5's installed CMake config with hdf5-shared library names
+    def cmake_args(self):
+        options = super(Openfast, self).cmake_args()
+        options.append('-DHDF5_NO_FIND_PACKAGE_CONFIG_FILE:BOOL=ON')
+        return options
