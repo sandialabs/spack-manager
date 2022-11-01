74a71a65b99ae02cee5645e23f510b19a01279dd
Jon Rood <jon.rood@nrel.gov>
2022-11-01 13:34:46 -0600

    Fix accidental changes.

--- repos/exawind/packages/hdf5/package.py
+++ repos/exawind/packages/hdf5/package.py
@@ -13,7 +13,7 @@ class Hdf5(bHdf5):
     def symlink_to_h5hl_wrappers(self):
         if self.spec.satisfies("@1.8.21:1.8.22,1.10.2:1.10.7,1.12.0+hl"):
             with working_dir(self.prefix.bin):
-                # CMake"s FindHDF5 relies only on h5cc so it doesn't find the HL component unless it uses h5hlcc
+                # CMake's FindHDF5 relies only on h5cc so it doesn't find the HL component unless it uses h5hlcc
                 # so we symlink h5cc to h5hlcc etc
                 os.remove("h5cc")
                 os.remove("h5c++")
