5eb58d161d682de8649ee1e418efbb187be5e05a
Jon Rood <jon.rood@nrel.gov>
2022-11-21 15:04:04 -0600

    Use hostname in exawind-nightly. (#439)

--- repos/exawind/packages/exawind-nightly/package.py
+++ repos/exawind/packages/exawind-nightly/package.py
@@ -13,6 +13,8 @@ from shutil import copyfile
 import inspect
 import re
 from spack.util.executable import ProcessError
+import manager_cmds.find_machine as fm
+from manager_cmds.find_machine import find_machine
 
 class ExawindNightly(bExawind):
     """Extension of exawind for nightly build and test"""
@@ -28,8 +30,15 @@ class ExawindNightly(bExawind):
         cmake_options += self.cmake_args()
         cmake_options.remove("-G")
         cmake_options.remove("Unix Makefiles") # The space causes problems for ctest
+
+        machine = find_machine(verbose=False)
+        full_machine = find_machine(verbose=False, full_machine_name=True)
         if spec.variants["host_name"].value == "default":
-            spec.variants["host_name"].value = spec.format("{architecture}")
+            if full_machine == "NOT-FOUND":
+                spec.variants["host_name"].value = spec.format("{architecture}")
+            else:
+                spec.variants["host_name"].value = full_machine
+
         options.append(self.define("CMAKE_CONFIGURE_ARGS"," ".join(v for v in cmake_options)))
         options.append(self.define("CTEST_SOURCE_DIRECTORY", self.stage.source_path))
         options.append(self.define("CTEST_BINARY_DIRECTORY", self.build_directory))
