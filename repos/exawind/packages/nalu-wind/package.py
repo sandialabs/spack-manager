9cfb34a1b269cc0cd7b376412f8ee2ae116fe79d
psakievich <psakiev@sandia.gov>
2023-02-06 13:19:55 -0700

    Require ofast c++ bindings with +fsi

--- repos/exawind/packages/nalu-wind/package.py
+++ repos/exawind/packages/nalu-wind/package.py
@@ -45,7 +45,7 @@ class NaluWind(bNaluWind, ROCmPackage):
     depends_on("hypre+umpire", when="+umpire")
 
     depends_on("trilinos gotype=long")
-    depends_on("openfast@fsi+netcdf", when="+fsi")
+    depends_on("openfast@fsi+netcdf+cxx", when="+fsi")
 
     for _arch in ROCmPackage.amdgpu_targets:
         depends_on("trilinos@13.4.0.2022.10.27: ~shared+exodus+tpetra+zoltan+stk+boost~superlu-dist~superlu+hdf5+shards~hypre+gtest+rocm amdgpu_target={0}".format(_arch),
