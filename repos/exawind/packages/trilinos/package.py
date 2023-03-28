db114001b33ed9699a0941d1cd19ec293f566a73
Timothy Smith <58484958+tasmith4@users.noreply.github.com>
2023-03-28 14:26:56 -0600

    trilinos: fix ending version for STK patch on eagle (#512)

--- repos/exawind/packages/trilinos/package.py
+++ repos/exawind/packages/trilinos/package.py
@@ -35,7 +35,7 @@ class Trilinos(bTrilinos, SMCMakeExtension):
 
     machine = find_machine(verbose=False, full_machine_name=False)
     if machine == "eagle":
-        patch("stk-coupling-versions-func-overload.patch", when="@13.3.0:13.5.0.2022.12.15")
+        patch("stk-coupling-versions-func-overload.patch", when="@13.3.0:13.4.0.2022.12.15")
 
     def setup_build_environment(self, env):
         spec = self.spec
