66187c513934bc25d5c7113962a3fc96efa8383d
psakievich <psakiev@sandia.gov>
2023-06-28 13:58:25 -0600

    Make views default to false

--- spack-scripting/scripting/cmd/manager_cmds/create_env.py
+++ spack-scripting/scripting/cmd/manager_cmds/create_env.py
@@ -40,7 +40,7 @@ def create_env(parser, args):
     if args.yaml:
         assert os.path.isfile(args.yaml)
 
-    environment.create_in_dir(theDir, init_file=args.yaml, keep_relative=True)
+    environment.create_in_dir(theDir, init_file=args.yaml, keep_relative=True, with_view=False)
 
     manifest = SpackManagerEnvironmentManifest(theDir)
 
