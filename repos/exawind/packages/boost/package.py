1da098495e31ba1b2d51a452d856e8f8201583cc
Jon Rood <jon.rood@nrel.gov>
2023-08-17 10:39:04 -0600

    Fix boost and add rpaths for intel compilers.

--- repos/exawind/packages/boost/package.py
+++ repos/exawind/packages/boost/package.py
@@ -11,3 +11,5 @@ from spack.pkg.builtin.boost import Boost as bBoost
 class Boost(bBoost):
     # Avoid the build system not finding static -lm -ldl etc
     patch("intel-no-static.patch", when="@1.76.0:%intel")
+    patch("intel-no-static.patch", when="@1.76.0:%oneapi")
+    patch("intel-no-static.patch", when="@1.76.0:%dpcpp")
