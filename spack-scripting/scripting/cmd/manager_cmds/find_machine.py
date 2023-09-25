6c071b6d04d5de2b72a30c36efe65d7322feb67f
Amy J. Powell <ajpowel@sandia.gov>
2023-09-25 11:28:46 -0700

    find_machine.py: add CONTAINER_BUILD=cpu

--- spack-scripting/scripting/cmd/manager_cmds/find_machine.py
+++ spack-scripting/scripting/cmd/manager_cmds/find_machine.py
@@ -100,7 +100,7 @@ machine_list = {
     # General
     "darwin": MachineData(lambda: sys.platform == "darwin", "darwin.nodomain.gov"),
     "containercpu": MachineData(
-        lambda: os.environ["SPACK_MACHINE_MANAGER"] == "containercpu", "containcpu.nodomain.gov"
+        lambda: os.environ["CONTAINER_BUILD"] == "cpu", "containcpu.nodomain.gov"
     ),
 }
 
