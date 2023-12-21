ea6898ae6b36f83d169d24b77e182e90fab2b349
Paul Mullowney <Paul.Mullowney@amd.com>
2023-12-21 12:45:33 -0500

    Adding roctx amrex hooks

--- repos/exawind/packages/amr-wind/package.py
+++ repos/exawind/packages/amr-wind/package.py
@@ -29,7 +29,8 @@ class AmrWind(SMCMakeExtension, bAmrWind):
             description="Enable SYCL backend")
     variant("gpu-aware-mpi", default=False,
             description="gpu-aware-mpi")
-
+    variant("roctx-profile", default=False,
+            description="do profiling with roctx")
     depends_on("hdf5~mpi", when="+hdf5~mpi")
     depends_on("hdf5+mpi", when="+hdf5+mpi")
     depends_on("h5z-zfp", when="+hdf5")
@@ -104,6 +105,8 @@ class AmrWind(SMCMakeExtension, bAmrWind):
             cmake_options.append("-DCMAKE_HIP_ARCHITECTURES=" + ";".join(str(x) for x in targets))
             cmake_options.append("-DAMDGPU_TARGETS=" + ";".join(str(x) for x in targets))
             cmake_options.append("-DGPU_TARGETS=" + ";".join(str(x) for x in targets))
+            if "+roctx-profile" in self.spec:
+                cmake_options.append(self.define("AMReX_USE_ROCTX", True))
 
         if "+sycl" in self.spec:
             cmake_options.append(self.define("AMR_WIND_ENABLE_SYCL", True))
