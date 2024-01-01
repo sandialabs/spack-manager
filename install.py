2997d62c4e92cbae003c076f41e35f9bf424789a
psakiev <psakiev@sandia.gov>
2024-01-01 09:12:53 -0700

    Add installation procedure

---
+++ install.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env spack-python
+# Copyright (c) 2022, National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# This software is released under the BSD 3-clause license. See LICENSE file
+# for more details.
+"""
+This is the installation script for spack-manager.
+It can be called for multiple instances of spack and simply registers
+the extension with spack.
+"""
+
+import argparse
+import os
+import spack.main
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--scope", required=False, help="Spack scope to register spack-manager")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    register_args = []
+
+    if args.scope:
+        register_args.extend(["--scope", args.scope])
+
+    register_args.extend(
+        ["add", "config", "extensions:[{0}]".format(os.path.dirname(__file__))]
+    )
+
+    config = spack.main.SpackCommand("config")
+    config(*register_args)
