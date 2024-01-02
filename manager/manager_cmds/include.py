e91d8f043f05be455c5cceb4911281072a6330c4
psakiev <psakiev@sandia.gov>
2024-01-01 23:02:17 -0700

    Clean up

--- manager/manager_cmds/include.py
+++ manager/manager_cmds/include.py
@@ -10,7 +10,6 @@ import sys
 
 import llnl.util.tty as tty
 
-import spack.extensions.manager as manager
 import spack.extensions.manager.projects as projects
 from spack.extensions.manager.manager_cmds.find_machine import find_machine, machine_defined
 from spack.extensions.manager.manager_cmds.includes_creator import IncludesCreator
