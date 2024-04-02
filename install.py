0985ac48c0be3603b6ea140a55d1eaf924cae9bc
psakiev <psakiev@sandia.gov>
2024-04-02 09:30:38 -0600

    Require python 3.8 or higher

--- install.py
+++ install.py
@@ -15,6 +15,9 @@ import argparse
 import llnl.util.tty as tty
 import os
 import spack.main
+import importlib
+
+check = importlib.import_moduel("check")
 
 
 parser = argparse.ArgumentParser()
@@ -22,6 +25,7 @@ parser.add_argument("-s", "--scope", required=False, help="Spack scope to regist
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    check.check_spack_manager_requirements()
 
     extension_path = os.path.realpath(os.getcwd())
     if not os.path.isdir(os.path.join(extension_path, "manager")):
