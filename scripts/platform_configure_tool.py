28707a6b516c95ace891aeeb726cfb0877f92fda
psakievich <psakiev@sandia.gov>
2024-04-15 09:57:22 -0600

    Update platform_configure_tool.py

--- scripts/platform_configure_tool.py
+++ scripts/platform_configure_tool.py
@@ -10,14 +10,21 @@ from spack.util.module_cmd import module
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="input file describing what to configure")
-parser.add_argument("--output", help="location where the configs should get written")
+output_types = parser.add_mutually_exclusive_group(required=True)
+output_types.add_argument("--output", help="location where the configs should get written")
+output_types.add_argument("--scope", help="spack scope where the configs should get written")
 
 args = parser.parse_args()
 
 input_path = pathlib.PurePath(args.input)
-output_path = pathlib.PurePath(args.output)
-
-os.environ["SPACK_USER_CONFIG_PATH"] = str(output_path)
+scope_tempalate = "--scope={}"
+if args.output:
+    output_path = pathlib.PurePath(args.output)
+    os.environ["SPACK_USER_CONFIG_PATH"] = str(output_path)
+    scope = scope_template.format("user")
+else:
+    scope = scope_template.format(args.scope)
+    
 exe_env = os.environ.copy()
 
 with open(input_path, "r") as f:
@@ -29,7 +36,7 @@ external_cmd = spack.main.SpackCommand("external", subprocess=True)
 if "compilers" in manifest:
     for c in manifest["compilers"]:
         module("load", c)
-        print(compiler("find", "--scope=user", env=exe_env))
+        print(compiler("find", scope, env=exe_env))
         module("unload", c)
 
 if "externals" in manifest:
