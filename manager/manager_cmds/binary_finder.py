81fbe8bd59bcd57e09b7dbdade4fce4b2794160d
Philip Sakievich <psakiev@sandia.gov>
2024-05-17 15:07:52 -0600

    Add tools for querying and comparing environments

---
+++ manager/manager_cmds/binary_finder.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2022, National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# This software is released under the BSD 3-clause license. See LICENSE file
+# for more details.
+
+
+import spack.main
+import spack.environment as ev
+import time
+
+command_name = "binary-finder"
+
+def setup_parser_args(subparser):
+    subparser.add_argument(
+            "--source",
+            "-s",
+            choices=["cache", "upstream", "both"],
+            default="both",
+            help="source where hits can come from"
+            )
+    subparser.add_argument(
+            "--output",
+            "-o",
+            choices=["miss", "hit", "both"],
+            default="miss",
+            help="what type of cache queries are output",
+            )
+
+
+def binary_finder(parser, args):
+    env = spack.cmd.require_active_env(cmd_name=command_name)
+    hashes = env.all_hashes()
+    n = len(hashes)
+    # TODO clean misc cache
+
+    print(f"Querying buildcache for {n} specs")
+    misses = []
+    hits = []
+
+    cache_contents = []
+    def cache_hit(h):
+        return h in cache_contents
+
+    def upstream_hit(spec):
+        return spec.installed_upstream
+
+    for i, h in enumerate(hashes):
+        # serial loop over all the hashes, would be commented out if using a Pool
+        spec = env.get_one_by_hash(h)
+
+        hit = False
+        if args.source != "upstream":
+            hit = hit or cache_hit(h)
+        if args.source != "cache":
+            hit = hit or upstream_hit(spec)
+
+        if hit:
+            hits.append((h, spec))
+        else:
+            misses.append((h, spec))
+
+        j=i+1
+        time.sleep(0.01)
+        if j != n:
+            print(f"\r-- {j}/{n}", end="\r")
+        else:
+            print(f"\r-- {j}/{n}", end="\n")
+
+    if args.output == "miss" or args.output == "both":
+        print("----------------------------------------")
+        print("Cache Miss Legend")
+        print("[E]: External")
+        print("[D]: Develop Spec")
+        print("[-]: True Miss")
+        print("----------------------------------------")
+        print("\n----------------------------------------")
+        print("The following {} specs missed in the cache:".format(len(misses)))
+        print("----------------------------------------")
+        def miss_reason(spec):
+            template = "[{}]"
+            if env.is_develop(spec):
+                return template.format("D")
+            elif spec.external:
+                return template.format("E")
+            elif spec.installed_upstream:
+                return template.format("U")
+            else:
+                return template.format("-")
+
+        for miss in misses:
+            h, s = miss
+            mark = miss_reason(s)
+            spec_name = s.name
+            print(f"/{h}: {mark} {s.name}")
+
+    if args.output == "hit" or args.output == "both":
+        print("\n----------------------------------------")
+        print("The following {} specs hit in the cache:".format(len(hits)))
+        print("----------------------------------------")
+        for hit in hits:
+            h, s = hit
+            spec_name = s.name
+            print(f"/{h}: [+] {s.name}")
+
+    print("\n----------------------------------------")
+    print("Summary: Hits ({}) Misses ({})".format(len(hits), len(misses)))
+    print("----------------------------------------")
+
+
+def add_command(parser, command_dict):
+    subparser = parser.add_parser(
+            command_name,
+            help="check upstreams and binary caches for hits on a concretized environment",
+    )
+    setup_parser_args(subparser)
+    command_dict[command_name] = binary_finder
---
+++ manager/manager_cmds/lock_diff.py
@@ -0,0 +1,140 @@
+# Copyright (c) 2022, National Technology & Engineering Solutions of Sandia,
+# LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+#
+# This software is released under the BSD 3-clause license. See LICENSE file
+# for more details.
+
+
+"""
+This tool will diff two concrete environments to analyze
+for concretization differences
+
+To use supply the location of two spack.lock files to the script
+"""
+import argparse
+import os
+import random
+import string
+import sys
+
+import spack.environment as ev
+
+from spack.spec import Spec
+
+command_name = "lock-diff"
+
+def setup_parser_args(subparser):
+    subparser.add_argument("--old", "-o", help="path to the older spack.lock", required=True)
+    subparser.add_argument("--new", "-n", help="path to the newer/updated spack.lock", required=True)
+    subparser.add_argument("--skip-package-diffs", "-s", nargs="+", help="packages to skip package hash diffs", required=False)
+
+
+def lock_diff(parser, args):
+    file_bad = False
+    if not os.path.isfile(args.old):
+        tty.error(f"{args.old} lock files is not a valid file")
+        file_bad = True
+    if not os.path.isfile(args.new):
+        tty.error(f"{args.new} lock files is not a valid file")
+        file_bad = True
+    if file_bad:
+        sys.exit(1)
+
+    # create randomly named env's
+    def generate_random_name(num_chars=10, valid_chars=string.ascii_lowercase):
+        return "".join(random.choice(valid_chars) for _ in range(num_chars))
+
+    e_old_name = generate_random_name()
+    e_new_name = generate_random_name()
+
+    e_old = ev.create(e_old_name, init_file=args.old)
+    e_new = ev.create(e_new_name, init_file=args.new)
+
+    # since they are lock files should be a psuedo no-op
+    e_old.concretize()
+    e_new.concretize()
+
+    # different specs with the same name can be in both env if the hash's change.
+    # reduce to a single instance per spec using a dictionary
+
+    spec_differences = {}
+    for s in set(e_old.all_specs()) ^ set(e_new.all_specs()):
+        spec_differences[s.name] = s
+
+    def keys_of_diffing_dictionary_values(d1, d2):
+        """find keys where values differ for two identically keyed dictionaries"""
+        assert d1.keys() == d2.keys()
+        pre_out = []
+        for item in d1.items() ^ d2.items():
+            pre_out.append(item[0])
+        return set(pre_out)
+
+    def old_vs_new(old_dict, new_dict):
+        """extract the differening values of two dictionaries as two separate strings"""
+        old = []
+        new = []
+        for key in keys_of_diffing_dictionary_values(old_dict, new_dict):
+            old.append(str(old_dict[key]))
+            new.append(str(new_dict[key]))
+        return " ".join(old), " ".join(new)
+
+    # loop over different specs and diagnose what has changed
+    # kind of hacky check to see if we have acceptable diffs. worth a refactor someday
+    unacceptable_changes = False
+    if spec_differences:
+        print("Differences in Following Specs Detected:")
+        for spec in spec_differences.values():
+            old_spec = e_old.matching_spec(Spec(spec.name))
+            new_spec = e_new.matching_spec(Spec(spec.name))
+            diff_msg = "- {}: ".format(spec.name)
+            if old_spec and not new_spec:
+                diff_msg += "*removed*"
+                unacceptable_changes = True
+            elif not old_spec and new_spec:
+                diff_msg += "*added*"
+                unacceptable_changes = True
+            elif new_spec.package_hash() != old_spec.package_hash():
+                # this will detect when we make changes in spack, or in our repos
+                diff_msg += "package hash change - Old: {} New: {}".format(old_spec.package_hash(), new_spec.package_hash())
+                if new_spec.name not in args.skip_package_diffs:
+                    unacceptable_changes = True
+            else:
+                # something else must have changed in concretization that should be avoided if we don't have reuse
+                # these are the things that we want to avoid
+                # if we get here then there is a more subtle issue
+                unacceptable_changes = True
+                assert old_spec.dag_hash() != new_spec.dag_hash()
+                diff_msg += "DAG hash change: {} {}".format(old_spec.dag_hash(), new_spec.dag_hash())
+                # TODO diff versions, compilers, variants, compiler flags in that order and print diagnostics
+                if old_spec.compiler != new_spec.compiler:
+                    diff_msg += "\n *compiler diff - \n\tOld: {}\n\tNew: {}".format(old_spec.compiler, new_spec.compiler)
+                if old_spec.version != new_spec.version:
+                    diff_msg += "\n *version diff - \n\tOld: {}\n\tNew: {}".format(old_spec.version, new_spec.version)
+                if old_spec.variants != new_spec.variants:
+                    # since package hash is the same, varints should be the same too
+                    old_vars, new_vars = old_vs_new(old_spec.variants, new_spec.variants)
+                    diff_msg += "\n *variants diff - \n\tOld: {}\n\tNew: {}".format(old_vars, new_vars)
+                if old_spec.compiler_flags != new_spec.compiler_flags:
+                    old_flags, new_flags = str(old_spec.compiler_flags), str(new_spec.compiler_flags)
+                    diff_msg += "\n *compiler_flags diff -\n\tOld {}\n\tNew: {}".format(old_flags, new_flags)
+            print(diff_msg)
+    else:
+        print("Environments are exactly the same")
+
+    # cleanup artifacts
+    e_old.destroy()
+    e_new.destroy()
+    if spec_differences and unacceptable_changes:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+def add_command(parser, command_dict):
+    subparser = parser.add_parser(
+            command_name,
+            help="diff two lock files to see how the environments differ",
+    )
+    setup_parser_args(subparser)
+    command_dict[command_name] = lock_diff
+
