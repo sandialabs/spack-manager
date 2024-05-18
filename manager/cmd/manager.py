b35930d79367d1c2e82dd20d6bd176240b8c565c
psakievich <psakiev@sandia.gov>
2024-05-17 21:41:58 -0600

    Add tools for querying and comparing environments (#607)
    * Add tools for querying and comparing environments
    
    * Run style
    
    * Fix binary finder and description
    
    * Add aliases
    
    * Style

--- manager/cmd/manager.py
+++ manager/cmd/manager.py
@@ -7,6 +7,7 @@
 
 import sys
 
+import spack.extensions.manager.manager_cmds.binary_finder as binary_finder
 import spack.extensions.manager.manager_cmds.cache_query as cache_query
 import spack.extensions.manager.manager_cmds.cli_config as cli_config
 import spack.extensions.manager.manager_cmds.create_dev_env as create_dev_env
@@ -16,13 +17,12 @@ import spack.extensions.manager.manager_cmds.external as external
 import spack.extensions.manager.manager_cmds.find_machine as find_machine
 import spack.extensions.manager.manager_cmds.include as include
 import spack.extensions.manager.manager_cmds.location as location
+import spack.extensions.manager.manager_cmds.lock_diff as lock_diff
 import spack.extensions.manager.manager_cmds.make as make
 import spack.extensions.manager.manager_cmds.pin as pin
 
-# import spack.extensions.manager.manager_cmds.snapshot as snapshot
-
-if sys.version_info[0] < 3:
-    print("spack-manager commands only support python 3")
+if sys.version_info < (3, 8):
+    print("spack-manager commands only supported in python 3.8 and higher")
     exit(1)
 
 description = "commands that are specific to spack-manager"
@@ -39,18 +39,18 @@ def setup_parser(subparser):
     cli_config.cli_commands["remove"](sp, _subcommands)
     cli_config.cli_commands["list"](sp, _subcommands)
 
+    binary_finder.add_command(sp, _subcommands)
     cache_query.add_command(sp, _subcommands)
     create_env.add_command(sp, _subcommands)
     create_dev_env.add_command(sp, _subcommands)
     develop.add_command(sp, _subcommands)
+    lock_diff.add_command(sp, _subcommands)
     external.add_command(sp, _subcommands)
     find_machine.add_command(sp, _subcommands)
     include.add_command(sp, _subcommands)
     location.add_command(sp, _subcommands)
     pin.add_command(sp, _subcommands)
     make.add_command(sp, _subcommands)
-    # pin.add_command(sp, _subcommands)
-    # snapshot.add_command(sp, _subcommands)
 
 
 def manager(parser, args):
