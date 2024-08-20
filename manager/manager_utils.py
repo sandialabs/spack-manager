37bdf330b40e1d544144342c671a1c7f2913b909
psakievich <psakiev@sandia.gov>
2024-08-20 14:49:40 -0600

    Omit arch from external spec reconstruction

--- manager/manager_utils.py
+++ manager/manager_utils.py
@@ -34,7 +34,7 @@ def canonicalize_path(path, default_wd=None):
 
 
 def pruned_spec_string(spec, variants_to_omit=["ipo", "dev_path=", "patches=", "build_system="]):
-    full_spec = spec.format("{name}{@version}{%compiler}{variants}{arch=architecture}")
+    full_spec = spec.format("{name}{@version}{%compiler}{variants}")
 
     # add spaces between variants so we can filter
     spec_components = full_spec.replace("+", " +").replace("~", " ~").split(" ")
