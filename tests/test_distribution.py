5db64319bf4f2319c0b17520b51c248ca4f9cc06
Jacob Healy <jnhealy@sandia.gov>
2025-09-09 11:16:08 -0600

    Fix bug in distribution command
    The distribution command was modifying the list of repos in a given
    spack environment which causes problems when resolving package dependencies.
    Maintain the original package repository data structure throughout the
    distro bundling process so that this does not occur.

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -8,6 +8,7 @@
 import inspect
 import os
 from argparse import ArgumentParser
+from collections import OrderedDict
 
 import spack
 import spack.cmd.bootstrap as test_bootstrap_parse
@@ -52,7 +53,7 @@ def {base_name}(parser, args):
 
 def create_repo(path):
     os.makedirs(path)
-    data = {"repo": {"namespace": "test"}}
+    data = {"repo": {"namespace": os.path.basename(path)}}
     with open(os.path.join(path, "repo.yaml"), "w") as f:
         spack.util.spack_yaml.dump(data, f, default_flow_style=False)
     package = os.path.join(path, "packages", "test")
@@ -416,10 +417,17 @@ def test_DistributionPackager_configure_repos(tmpdir):
     the environment into the environment being created and adds a relative path to the
     copied repositories to the spack.yaml that is being constructed.
     """
-    package_repo = os.path.join(tmpdir.strpath, "mock-repo")
+    package_repo = os.path.join(tmpdir.strpath, "mock_repo")
     create_repo(package_repo)
 
-    extra_data = {"repos": {"mock-repo": package_repo}}
+    package_repo2 = os.path.join(tmpdir.strpath, "mock_repo2")
+    create_repo(package_repo2)
+
+    data = OrderedDict()
+    data["mock_repo"] = package_repo
+    data["mock_repo2"] = package_repo2
+
+    extra_data = {"repos": data}
     manifest = os.path.join(tmpdir.strpath, "base-env", "spack.yaml")
     create_spack_manifest(manifest, extra_data=extra_data)
     env = spack.environment.Environment(os.path.dirname(manifest))
@@ -436,8 +444,11 @@ def test_DistributionPackager_configure_repos(tmpdir):
     assert os.path.isdir(expected_repo_dir)
     assert "repos" in result_config["spack"]
 
-    expected_repo = {"mock-repo": f"../{os.path.basename(pkgr.package_repos)}/mock-repo"}
-    assert expected_repo == result_config["spack"]["repos"]
+    expected = OrderedDict()
+    for key, val in data.items():
+        expected[key] = f"../{os.path.basename(pkgr.package_repos)}/{os.path.basename(val)}"
+
+    assert expected == result_config["spack"]["repos"]
 
 
 def test_DistributionPackager_configure_package_settings(tmpdir):
